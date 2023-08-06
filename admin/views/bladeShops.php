2a54d51b823286f6438d756bd52e33db71dde43c
MRxwr <nasserhatab@gmail.com>
2023-08-07 00:46:32 +0300

    build shops module

---
+++ admin/views/bladeShops.php
@@ -0,0 +1,96 @@
+<div class="col-sm-12">
+<div class="panel panel-default card-view">
+<div class="panel-heading">
+<div class="pull-left">
+	<h6 class="panel-title txt-dark"><?php echo direction("Shop Details","تفاصيل المحل") ?></h6>
+</div>
+	<div class="clearfix"></div>
+</div>
+<div class="panel-wrapper collapse in">
+<div class="panel-body">
+	<form class="" method="POST" action="" enctype="multipart/form-data">
+		<div class="row m-0">
+			<div class="col-md-6">
+			<label><?php echo direction("English Title","الإسم الإنجليزي") ?></label>
+			<input type="text" name="enTitle" class="form-control" required>
+			</div>
+			
+			<div class="col-md-6">
+			<label><?php echo direction("Arabic Title","الإسم العربي") ?></label>
+			<input type="text" name="arTitle" class="form-control" required>
+			</div>
+			
+			<div class="col-md-6" style="margin-top:10px">
+			<input type="submit" class="btn btn-primary" value="<?php echo direction("Submit","أرسل") ?>">
+			<input type="hidden" name="update" value="0">
+			</div>
+		</div>
+	</form>
+</div>
+</div>
+</div>
+</div>
+				
+				<!-- Bordered Table -->
+<div class="col-sm-12">
+<div class="panel panel-default card-view">
+<div class="panel-heading">
+<div class="pull-left">
+<h6 class="panel-title txt-dark"><?php echo direction("List of Shops","قائمة المحلات") ?></h6>
+</div>
+<div class="clearfix"></div>
+</div>
+<div class="panel-wrapper collapse in">
+<div class="panel-body">
+<div class="table-wrap mt-40">
+<div class="table-responsive">
+	<table class="table display responsive product-overview mb-30" id="myTable">
+		<thead>
+		<tr>
+		<th><?php echo direction("English Title","الإسم الإنجليزي") ?></th>
+		<th><?php echo direction("Arabic Title","الإسم العربي") ?></th>
+		<th class="text-nowrap"><?php echo direction("Actions","الخيارات") ?></th>
+		</tr>
+		</thead>
+		
+		<tbody>
+		<?php 
+		if( $shops = selectDB("shops","`status` = '0'") ){
+			for( $i = 0; $i < sizeof($shops); $i++ ){
+				?>
+				<tr>
+				<td id="enTitle<?php echo $shops[$i]["id"]?>" ><?php echo $shops[$i]["enTitle"] ?></td>
+				<td id="arTitle<?php echo $shops[$i]["id"]?>" ><?php echo $shops[$i]["arTitle"] ?></td>
+				<td class="text-nowrap">
+				
+				<a id="<?php echo $shops[$i]["id"] ?>" class="mr-25 edit" data-toggle="tooltip" data-original-title="Edit"> <i class="fa fa-pencil text-inverse m-r-10"></i>
+				</a>
+
+				<a href="?delId=<?php echo $shops[$i]["id"] . "&v={$_GET["v"]}" ?>" data-toggle="tooltip" data-original-title="Delete"><i class="fa fa-close text-danger"></i>
+				</a>
+				
+				</td>
+				</tr>
+				<?php
+			}
+		}
+		?>
+		</tbody>
+		
+	</table>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+	<script>
+		$(document).on("click",".edit", function(){
+			var id = $(this).attr("id");
+			var enTitle = $("#enTitle"+id).html();
+			var arTitle = $("#arTitle"+id).html();
+			$("input[name=enTitle]").val(enTitle);
+			$("input[name=arTitle]").val(arTitle);
+			$("input[name=update]").val(id);
+		})
+	</script>
