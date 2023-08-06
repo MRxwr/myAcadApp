715e2a2617f6756f80ee310fd809a8ae1c604e9a
MRxwr <nasserhatab@gmail.com>
2023-08-07 00:51:47 +0300

    Update bladeRoles.php

--- admin/views/bladeRoles.php
+++ admin/views/bladeRoles.php
@@ -22,6 +22,7 @@
 			
 			<div class="col-md-6" style="margin-top:10px">
 			    <input type="submit" class="btn btn-primary" value="<?php echo direction("Submit","أرسل") ?>">
+				<input type="hidden" name="update" value="0">
 			</div>
 		</div>
 	</form>
