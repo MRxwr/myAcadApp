f60aa5d64a4bb5d1453b52ec86f3c4d04836fd5a
MRxwr <nasserhatab@gmail.com>
2023-08-07 01:34:59 +0300

    Update bladeSocial_Media.php

--- admin/views/bladeSocial_Media.php
+++ admin/views/bladeSocial_Media.php
@@ -3,37 +3,39 @@ if ( $social_media = selectDB("social_media","`id` = '1'") ){
 	$array = ["whatsapp","snapchat","instagram","location","tiktok","email"];
 }
 ?>
-<div class="col-sm-12 col-xs-12">
-<div class="form-wrap">
-<form action="?update=1" method="POST">
-<div class="form-body">
-<h6 class="txt-dark capitalize-font">
-<i class="zmdi zmdi-account mr-10"></i><?php echo direction("Social Media Accounts", "حسابات السوشال ميديا") ?>
-</h6>
-<hr class="light-grey-hr"/>
-<div class="row">
-
-<?php 
-for( $i =0; $i < sizeof($array); $i++ ){
-?>
-	<div class="col-md-6">
-	<div class="form-group">
-	<label class="control-label mb-10"><?php echo strtoupper($array[$i]) ?></label>
-	<input type="text" name="<?php echo strtolower($array[$i]) ?>" class="form-control" value="<?php echo $social_media[0][$array[$i]] ?>"  >
+<div class="col-sm-12">
+<div class="panel panel-default card-view">
+<div class="panel-heading">
+<div class="pull-left">
+	<h6 class="panel-title txt-dark"><?php echo direction("Social Media Accounts", "حسابات السوشال ميديا") ?></h6>
+</div>
+	<div class="clearfix"></div>
+</div>
+<div class="panel-wrapper collapse in">
+<div class="panel-body">
+	<form class="" method="POST" action="" enctype="multipart/form-data">
+		<div class="row m-0">
+		<?php 
+		for( $i =0; $i < sizeof($array); $i++ ){
+		?>
+			<div class="col-md-6">
+			<div class="form-group">
+			<label class="control-label mb-10"><?php echo strtoupper($array[$i]) ?></label>
+			<input type="text" name="<?php echo strtolower($array[$i]) ?>" class="form-control" value="<?php echo $social_media[0][$array[$i]] ?>"  >
+			</div>
+			</div>
+		<?php
+		}
+		?>
+		<div class="col-md-12">
+		<div class="form-group">
+		<button type="submit" class="btn btn-primary w-50"><?php echo direction("Update","تعديل") ?></button>
+		<input type="hidden" name="update" class="form-control" value="1"  >
+		</div>
+		</div>
 	</div>
-	</div>
-<?php
-}
-?>
-<div class="col-md-12">
-<div class="form-group">
-<button type="submit" class="btn btn-primary w-50"><?php echo direction("Update","تعديل") ?></button>
-<input type="hidden" name="update" class="form-control" value="1"  >
+	</form>
 </div>
 </div>
-
-</div>
-</div>
-</form>
 </div>
 </div>
