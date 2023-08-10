02a1acd752788dca16a9618acbdb6c2461c50cc2
MRxwr <nasserhatab@gmail.com>
2023-08-11 00:47:39 +0300

    Update bladeUsers.php

--- admin/views/bladeUsers.php
+++ admin/views/bladeUsers.php
@@ -11,8 +11,13 @@
 	<form class="" method="POST" action="?v=<?php echo $_GET["v"] ?>" enctype="multipart/form-data">
 		<div class="row m-0">
 			<div class="col-md-3">
-			<label><?php echo direction("Name","الإسم") ?></label>
-			<input type="text" name="fullName" class="form-control" required>
+			<label><?php echo direction("First Name","الإسم الأول") ?></label>
+			<input type="text" name="firstName" class="form-control" required>
+			</div>
+
+			<div class="col-md-3">
+			<label><?php echo direction("Last Name","الإسم الأخير") ?></label>
+			<input type="text" name="lastName" class="form-control" required>
 			</div>
 			
 			<div class="col-md-3">
@@ -55,7 +60,8 @@
 	<table class="table display responsive product-overview mb-30" id="myTable">
 		<thead>
 		<tr>
-		<th><?php echo direction("Name","الإسم") ?></th>
+		<th><?php echo direction("First Name","الإسم الأول") ?></th>
+		<th><?php echo direction("Last Name","الإسم الأخير") ?></th>
 		<th><?php echo direction("Email","الإيميل") ?></th>
 		<th><?php echo direction("Mobile","الهاتف") ?></th>
 		<th class="text-nowrap"><?php echo direction("Actions","الخيارات") ?></th>
@@ -64,30 +70,31 @@
 		
 		<tbody>
 		<?php 
-		if( $employees = selectDB("users","`status` = '0' AND `hidden` != '2'") ){
-			for( $i = 0; $i < sizeof($employees); $i++ ){
+		if( $users = selectDB("users","`status` = '0' AND `hidden` != '2'") ){
+			for( $i = 0; $i < sizeof($users); $i++ ){
 				$counter = $i + 1;
-				if ( $employees[$i]["hidden"] == 1 ){
+				if ( $users[$i]["hidden"] == 1 ){
 					$icon = "fa fa-unlock";
-					$link = "?v={$_GET["v"]}&show={$employees[$i]["id"]}";
+					$link = "?v={$_GET["v"]}&show={$users[$i]["id"]}";
 					$hide = direction("Unlock","فتح الحساب");
 				}else{
 					$icon = "fa fa-lock";
-					$link = "?v={$_GET["v"]}&hide={$employees[$i]["id"]}";
+					$link = "?v={$_GET["v"]}&hide={$users[$i]["id"]}";
 					$hide = direction("Lock","قفل الحساب");
 				}
 				
 				?>
 				<tr>
-				<td id="name<?php echo $employees[$i]["id"]?>" ><?php echo $employees[$i]["fullName"] ?></td>
-				<td id="email<?php echo $employees[$i]["id"]?>" ><?php echo $employees[$i]["email"] ?></td>
-				<td id="mobile<?php echo $employees[$i]["id"]?>" ><?php echo $employees[$i]["phone"] ?></td>
+				<td id="firstName<?php echo $users[$i]["id"]?>" ><?php echo $users[$i]["firstName"] ?></td>
+				<td id="lastName<?php echo $users[$i]["id"]?>" ><?php echo $users[$i]["lastName"] ?></td>
+				<td id="email<?php echo $users[$i]["id"]?>" ><?php echo $users[$i]["email"] ?></td>
+				<td id="mobile<?php echo $users[$i]["id"]?>" ><?php echo $users[$i]["phone"] ?></td>
 				<td class="text-nowrap">
-				<a id="<?php echo $employees[$i]["id"] ?>" class="mr-25 edit" data-toggle="tooltip" data-original-title="Edit"> <i class="fa fa-pencil text-inverse m-r-10"></i>
+				<a id="<?php echo $users[$i]["id"] ?>" class="mr-25 edit" data-toggle="tooltip" data-original-title="Edit"> <i class="fa fa-pencil text-inverse m-r-10"></i>
 				</a>
 				<a href="<?php echo $link ?>" class="mr-25" data-toggle="tooltip" data-original-title="<?php echo $hide ?>"> <i class="<?php echo $icon ?> text-inverse m-r-10"></i>
 				</a>
-				<a href="<?php echo "?v={$_GET["v"]}&delId=" . $employees[$i]["id"] ?>" data-toggle="tooltip" data-original-title="Delete"><i class="fa fa-close text-danger"></i>
+				<a href="<?php echo "?v={$_GET["v"]}&delId=" . $users[$i]["id"] ?>" data-toggle="tooltip" data-original-title="Delete"><i class="fa fa-close text-danger"></i>
 				</a>			
 				</td>
 				</tr>
@@ -108,13 +115,15 @@
 	$(document).on("click",".edit", function(){
 		var id = $(this).attr("id");
 		var email = $("#email"+id).html();
-		var name = $("#name"+id).html();
+		var fName = $("#firstName"+id).html();
+		var lName = $("#lastName"+id).html();
 		var mobile = $("#mobile"+id).html();
 		$("input[name=password]").prop("required",false);
 		$("input[name=email]").val(email);
 		$("input[name=phone]").val(mobile);
 		$("input[name=update]").val(id);
-		$("input[name=fullName]").val(name);
+		$("input[name=firstName]").val(fName);
+		$("input[name=lastName]").val(lName);
 		$("input[name=fullName]").focus();
 	})
 </script>
