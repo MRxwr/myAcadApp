88823dab22bdf88f751deffc95ab820fb534305d
MRxwr <nasserhatab@gmail.com>
2023-08-14 02:36:39 +0300

    Update header.php

--- admin/template/header.php
+++ admin/template/header.php
@@ -58,9 +58,6 @@ require ("payment.php");
 	
 	<!-- Custom CSS -->
 	<link href="dist/css/style.css" rel="stylesheet" type="text/css">
-
-	<!-- Include Select2 JS -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 </head>
 
 <body>
