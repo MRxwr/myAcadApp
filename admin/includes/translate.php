b6bcb039339f27f6b0ae8b9d0c4782bb11c1bee6
MRxwr <nasserhatab@gmail.com>
2023-08-15 03:16:28 +0300

    Update translate.php

--- admin/includes/translate.php
+++ admin/includes/translate.php
@@ -5,10 +5,10 @@ if ( isset($_GET["lang"]) ){
 		setcookie("CREATEkwLANG","{$_GET["lang"]}",(86400*30) + time(), "/");
 		header("Refresh:0 , url=" . str_replace("?lang={$_GET["lang"]}", "" ,str_replace("&lang={$_GET["lang"]}", "", $_SERVER['REQUEST_URI'])) );
 	}else{
-		setcookie("CREATEkwLANG","{$settingsLang}",(86400*30) + time(), "/");
-		header("Refresh:0 , url=" . str_replace("?lang={$settingsLang}", "" ,str_replace("&lang={$settingsLang}", "", $_SERVER['REQUEST_URI'])) );
+		setcookie("CREATEkwLANG","ENG",(86400*30) + time(), "/");
+		header("Refresh:0 , url=" . str_replace("?lang=ENG", "" ,str_replace("&lang=ENG", "", $_SERVER['REQUEST_URI'])) );
 	}
 }elseif( !isset($_COOKIE["CREATEkwLANG"]) ){
-	$_COOKIE["CREATEkwLANG"] = $settingsLang;
+	$_COOKIE["CREATEkwLANG"] = "ENG";
 }
 ?>
