f715923c4d17ba1aadade0441fe623723092393d
MRxwr <nasserhatab@gmail.com>
2023-08-15 03:35:10 +0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -59,7 +59,6 @@ if ( isset($_GET["hide"]) || isset($_GET["show"]) || isset($_GET["delId"]) || is
 			<?php
 			}
 		}else{
-            print_r($_POST);die();
             if( isset($_FILES['imageurl']) && is_uploaded_file($_FILES['imageurl']['tmp_name']) ){
                 $directory = "../logos/";
                 $originalfile = $directory . date("d-m-y") . time() .  round(microtime(true)). "L." . getFileExtension($_FILES["imageurl"]["name"]);
