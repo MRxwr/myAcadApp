c3fab0acb0946c93e48eabf396eefa169b74a308
MRxwr <nasserhatab@gmail.com>
2023-08-16 00:59:25 +0300

    header to navbar

--- template/header.php
+++ template/header.php
@@ -37,37 +37,3 @@ require_once("admin/includes/functions.php");
         <span></span>
     </div>
 </div>
-
-<header>
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-4 col-4">
-                <div class="header_lang">
-                    <div id="JLang" class="lang_flag">
-                        <div data-lang-code="en-GB" data-src="img/32/kuwait.png">English</div>
-                        <div data-lang-code="es-ES" data-src="img/32/spain.png">Español</div>
-                        <div data-lang-code="it-IT" data-src="img/32/Italy.png">Italian</div>
-                        <div data-lang-code="de-DE" data-src="img/32/Germany.png">Deutsche</div>
-                        <div data-lang-code="fr-FR" data-src="img/32/France.png">French</div>
-                    </div>
-                    <a href="#" class="button">Login</a>
-                </div>
-            </div>
-            <div class="col-lg-4 col-4 text-center">
-                <a href="index.html" class="logo">
-                    <img src="img/logo.png" alt="">
-                </a>
-            </div>
-            <div class="col-lg-4 col-4 d-flex align-items-center justify-content-end hum_gap">
-                <a href="rtl/index.html" class="button rtl_btn">Ar</a>
-                <!-- menu toggler -->
-                <div class="hamburger-menu">
-                    <span class="line-top"></span>
-                    <span class="line-center"></span>
-                    <span class="line-bottom"></span>
-                </div>
-            </div>  
-        </div>
-    </div>
-</header>
-<main class="overflow-hidden">
