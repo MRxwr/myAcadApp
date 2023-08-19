4351273c547824a25e7cb2cc4caff63da8317307
MRxwr <nasserhatab@gmail.com>
2023-08-19 08:43:10 +0300

    Update bannersSlider.php

--- template/bannersSlider.php
+++ template/bannersSlider.php
@@ -4,8 +4,8 @@
 		<?php
 		if( $banners = selectDB("banners","`status` = '0' AND `hidden` = '0' ORDER BY `order` ASC") ){
 			for( $i = 0; $i < sizeof($banners) ; $i++ ){
-				if( $banners[$i]["type"] == 1 ){
-					echo "<div class='item'><a href='{$banners[$i]["link"]}' target='' alt='Link-{$banners[$i]["title"]}'><img src='logos/{$banners[$i]["imageurl"]}' alt='{$banners[$i]["title"]}'></a></div>";
+				if( $banners[$i]["type"] == 0 ){
+					echo "<div class='item'><a href='{$banners[$i]["link"]}' target='_blank' alt='Link-{$banners[$i]["title"]}'><img src='logos/{$banners[$i]["imageurl"]}' alt='{$banners[$i]["title"]}'></a></div>";
 				}else{
 					echo "<div class='item'><div class='play_video' style='background-image: url('logos/{$banners[$i]["imageurl"]}');'><a href='{$banners[$i]["link"]}' class='watch_btn'><i class='fal fa-play-circle'></i></a></div>";
 				}
