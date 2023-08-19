4f1785c0b539c4564845ca2b698968a03c493b88
MRxwr <nasserhatab@gmail.com>
2023-08-19 08:37:20 +0300

    Update bannersSlider.php

--- template/bannersSlider.php
+++ template/bannersSlider.php
@@ -1,28 +1,17 @@
 <div class="carousel_area">
     <div class="container">
         <div class="owl-carousel slider1">
-            <div class="item">
-                <img src="img/ca_1.jpg" alt="">
-            </div>
-            <div class="item">
-                <img src="img/ca_2.jpg" alt="">
-            </div>
-            <div class="item">
-                <div class="play_video">
-                    <a href="https://www.youtube.com/watch?v=D0UnqGm_miA" class="watch_btn"><i class="fal fa-play-circle"></i></a>
-                </div>
-            </div>
-            <div class="item">
-                <img src="img/ca_1.jpg" alt="">
-            </div>
-            <div class="item">
-                <img src="img/ca_2.jpg" alt="">
-            </div>
-            <div class="item">
-                <div class="play_video">
-                    <a href="https://www.youtube.com/watch?v=D0UnqGm_miA" class="watch_btn"><i class="fal fa-play-circle"></i></a>
-                </div>
-            </div>
+		<?php
+		if( $banners = selectDB("banners","`status` = '0' AND `hidden` = '0' ORDER BY `order` ASC") ){
+			for( $i = 0; $i < sizeof($banners) ; $i++ ){
+				if( $banners[$i]["type"] == 1 ]){
+					echo "<div class='item'><a href='{$banners[$i]["link"]}' target='' alt='Link-{$banners[$i]["title"]}'><img src='logos/{$banners[$i]["imageurl"]}' alt='{$banners[$i]["title"]}'></a></div>";
+				}else{
+					echo "<div class='item'><div class='play_video' style='background-image: url('logos/{$banners[$i]["imageurl"]}');'><a href='{{$banners[$i]["link"]}}' class='watch_btn'><i class='fal fa-play-circle'></i></a></div>";
+				}
+			}
+		}
+		?>
         </div>
     </div>
 </div> 
