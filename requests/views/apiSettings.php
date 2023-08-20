dd30144095fa4cb67116562c72db780f1805ab7a
MRxwr <nasserhatab@gmail.com>
2023-08-21 01:50:23 +0300

    Update apiSettings.php

--- requests/views/apiSettings.php
+++ requests/views/apiSettings.php
@@ -1,5 +1,5 @@
 <?php 
-if( $settings = selectDB2("`version`, `enTerms`, `arTerms`, `enPolicy`, `arPolicy`","settings","`id` != '0'" ) && $social = selectDB2("`whatsapp`, `instagram`, `tiktok`, `snapchat`, `location`, `email`","social_media","`id` = '1'") ){
+if( $settings = selectDB2("`version`, `enTerms`, `arTerms`, `enPolicy`, `arPolicy`","settings","`id` = '1'" ) && $social = selectDB2("`whatsapp`, `instagram`, `tiktok`, `snapchat`, `location`, `email`","social_media","`id` = '1'") ){
 	$response["settings"] = $settings[0];
 	$response["social"] = $social[0];
 	echo outputData($response);
