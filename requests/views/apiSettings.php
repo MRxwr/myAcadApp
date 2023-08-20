14b03b2249d5824dccedbcb0b10ce8f94be0b884
MRxwr <nasserhatab@gmail.com>
2023-08-21 01:53:03 +0300

    Update apiSettings.php

--- requests/views/apiSettings.php
+++ requests/views/apiSettings.php
@@ -1,7 +1,12 @@
 <?php 
-$settings = selectDB2("`version`, `enTerms`, `arTerms`, `enPolicy`, `arPolicy`","settings","`id` = '1'" );
-$social = selectDB2("`whatsapp`, `instagram`, `tiktok`, `snapchat`, `location`, `email`","social_media","`id` = '1'");
-$response["settings"] = $settings;
-$response["social"] = $social[0];
-echo outputData($response);
+if( $settings = selectDB2("`version`, `enTerms`, `arTerms`, `enPolicy`, `arPolicy`","settings","`id` = '1'" ) ){
+	if( $social = selectDB2("`whatsapp`, `instagram`, `tiktok`, `snapchat`, `location`, `email`","social_media","`id` = '1'") ){
+		$response["social"] = $social[0];
+	}
+	$response["settings"] = $settings[0];
+	echo outputData($response);
+}else{
+	$error = array("msg"=>"Error while loading settings info");
+	echo outputError($error);die();
+}
 ?>
