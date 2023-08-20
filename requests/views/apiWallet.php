6120c2d0d43f13367bab4998ac39a9f518625911
MRxwr <nasserhatab@gmail.com>
2023-08-21 01:41:59 +0300

    Update apiWallet.php

--- requests/views/apiWallet.php
+++ requests/views/apiWallet.php
@@ -1,6 +1,6 @@
 <?php 
 if( $user = selectDB2("`wallet`","users","`id` = '{$_GET["userId"]}' " ) ){
-	echo outputData($user);
+	echo outputData($user[0]);
 }else{
 	$error = array("msg"=>"No user with this id");
 	echo outputError($error);die();
