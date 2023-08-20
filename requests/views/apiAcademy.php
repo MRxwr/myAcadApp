919bd11623940659386e3586c254b606517a850a
MRxwr <nasserhatab@gmail.com>
2023-08-21 01:32:57 +0300

    Update apiAcademy.php

--- requests/views/apiAcademy.php
+++ requests/views/apiAcademy.php
@@ -1,26 +1,26 @@
 <?php 
 if( !isset($_GET["academyId"]) || empty($_GET["academyId"]) ){
-	$response = array("msg"=>"Please set sport id");
+	$response = array("msg"=>"Please set academy id");
 	echo outputError($response);die();
 }else{
 	if( $academy = selectDB2("`id`, `imageurl`, `enTitle`, `arTitle`, `area`, `video`, `location`, `isClothes`, `clothesPrice`, `clothesImage`","academies","`hidden` = '0' AND `status` = '0' AND `id` = {$_GET["academyId"]}") ){
-		$response["academy"][0] = $academy[$i];
+		$response["academy"] = $academy;
 		if( $area = selectDB("countries","`id` = '{$academies[0]["area"]}'") ){
-			$response["academy"][0]["enArea"] = $area[0]["areaEnTitle"];
-			$response["academy"][0]["arArea"] = $area[0]["areaArTitle"];
+			$response["academy"]["enArea"] = $area[0]["areaEnTitle"];
+			$response["academy"]["arArea"] = $area[0]["areaArTitle"];
 		}else{
-			$response["academy"][0]["enArea"] = "";
-			$response["academy"][0]["arArea"] = "";
+			$response["academy"]["enArea"] = "";
+			$response["academy"]["arArea"] = "";
 		}
 		if( $sessions = selectDB("sessions","`academyId` = '{$academies[0]["area"]}'") ){
-			$response["sessions"][0] = $sessions;
+			$response["sessions"] = $sessions;
 		}else{
-			$response["sessions"][0] = array();
+			$response["sessions"] = array();
 		}
 		if( $subscriptions = selectDB("subscriptions","`academyId` = '{$academies[0]["area"]}'") ){
-			$response["subscriptions"][0] = $subscriptions;
+			$response["subscriptions"] = $subscriptions;
 		}else{
-			$response["subscriptions"][0] = array();
+			$response["subscriptions"] = array();
 		}
 	}else{
 		$response["msg"] = "there is no academy with this id";
