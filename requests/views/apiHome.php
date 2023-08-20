f2baf63954635f2f37d657cc221fb440cebd4620
MRxwr <nasserhatab@gmail.com>
2023-08-21 00:38:08 +0300

    Update apiHome.php

--- requests/views/apiHome.php
+++ requests/views/apiHome.php
@@ -7,10 +7,10 @@ if( $banners = selectDB2("`id`, `type`, `imageurl`, `link`","banners","`hidden` 
 }
 
 $response["genders"] = array(
-    int()1 => ["Man","رجل"],
-    int()2 => ["Woman","إمرأة"],
-    int()3 => ["Boy","ولد"],
-    int()4 => ["Girl","بنت"]
+    "man" => ["Man","رجل"],
+    "woman" => ["Woman","إمرأة"],
+    "boy" => ["Boy","ولد"],
+    "girl" => ["Girl","بنت"]
 );
 
 if( isset($_GET["countryCode"]) && $governates = selectDB2("`id`, `enTitle`, `arTitle`","governates","`hidden` = '0' AND `status` = '0' AND `countryCode` LIKE '{$_GET["countryCode"]}' ORDER BY `enTitle` ASC") ){
