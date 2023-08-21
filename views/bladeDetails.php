67478cafbf1b626601ae35f66a4c24f6e6f8ab73
MRxwr <nasserhatab@gmail.com>
2023-08-22 02:16:31 +0300

    Update bladeDetails.php

--- views/bladeDetails.php
+++ views/bladeDetails.php
@@ -90,7 +90,7 @@ $academy = $response["data"]["academy"];
         <div class="row justify-content-center">
             <div class="col-lg-10">
                 <h2><img src="img/map.svg" alt="">Location</h2>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3477.653725674415!2d47.90499537510941!3d29.351142351086718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3fcf8f9127be521b%3A0xb6d1e3a7bbbffa62!2sKuwait%20Dynamics!5e0!3m2!1sen!2sbd!4v1691060752363!5m2!1sen!2sbd"style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="<?php echo $academy["location"] ?>"style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </div>
