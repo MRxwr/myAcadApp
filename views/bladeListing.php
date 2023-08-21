c85e0b64a7b2261fef908f64952ad6e18b4e72ab
MRxwr <nasserhatab@gmail.com>
2023-08-22 01:58:37 +0300

    Update bladeListing.php

--- views/bladeListing.php
+++ views/bladeListing.php
@@ -30,7 +30,7 @@ if( $sportTitle = selectDB("sports","`id` = '{$_POST["sport"]}'") ){
         <h5><?php echo $sportTitle ?></h5>
         <div class="row">
 		<?php
-		if( $academies = $response["data"]["academies"] && $academies > 0 ){
+		if( isset($response["data"]["academies"]) && $academies = $response["data"]["academies"] && $academies > 0 ){
 			for( $i = 0; $i < sizeof($academies); $i++ ){
 				$counter = $i + 1;
 				if( $area = selectDB("countries","`id` = '{$academies[$i]["area"]}'") ){
