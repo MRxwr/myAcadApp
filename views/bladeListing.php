24e6fbbcbc78dd80416b1c5ddc9ce1845ee55fd3
MRxwr <nasserhatab@gmail.com>
2023-08-22 01:53:55 +0300

    Update bladeListing.php

--- views/bladeListing.php
+++ views/bladeListing.php
@@ -29,37 +29,52 @@ if( $sportTitle = selectDB("sports","`id` = '{$_POST["sport"]}'") ){
     <div class="container">
         <h5><?php echo $sportTitle ?></h5>
         <div class="row">
-
+		<?php
+		$academies = $response["data"]["academies"];
+		if( $academies > 0 ){
+			for( $i = 0; $i < sizeof($academies); $i++ ){
+				$counter = $i + 1;
+				if( $area = selectDB("countries","`id` = '{$academies[$i]["area"]}'") ){
+					$areaTitle = direction($area[0]["areaEnTitle"],$area[0]["arTitle"];
+				}else{
+					$areaTitle = "";
+				}
+		?>
             <div class="col-lg-4 col-sm-6 mt_50">
                 <div class="foott_box">
-                    <div class="promotion_box">
-                        <p>Promotion<span>30%</span></p>
-                    </div>
-                    <a href="#" class="s_foot_img">
-                        <img src="img/ca_2.jpg" alt="" class="w-100">
+				<?php 
+				if( $academies[$i]["isPromotion"] == 1 ){
+					echo "<div class='promotion_box'><p>".direction("Promotion","خصم")."</p></div>";
+				}
+				?>
+                    <a href="?v=Details&id=<?php echo $academies[$i]["id"] ?>" class="s_foot_img" alt="link_<?php echo $academies[$i]["enTitle"]?>">
+                        <img src="logos/<?php echo $academies[$i]["header"] ?>" alt="header_<?php echo $academies[$i]["enTitle"]?>" class="w-100">
                     </a>
                     <div class="foott_cont">
                         <div class="kuwat">
-                            <span>1</span>
+                            <span><?php echo $counter ?></span>
                             <div class="kuwat_items">
-                                <img src="img/kut_1.png" alt="">
+                                <img src="logos/<?php echo $academies[$i]["imageurl"] ?>" alt="logo_<?php echo $academies[$i]["enTitle"]?>">
                                 <div>
-                                    <h2>Dynamo Kuwait</h2>
-                                    <h3>Sabah Al Salem</h3>
+                                    <h2><?php echo direction($academies[$i]["enTitle"],$academies[$i]["arTitle"]) ?></h2>
+                                    <h3><?php echo $areaTitle ?></h3>
                                 </div>
                             </div>
                         </div>
                         <div class="last_rate">
-                            <h4>Rate</h4>
+                            <h4><?php echo direction("Rate","التقيم") ?></h4>
                             <div class="star_box">
                                 <img src="img/f_star.svg" alt="">
-                                <span>5.2</span>
+                                <span><?php echo $academies[$i]["قشفهىل"]?></span>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            
+        <?php
+			}
+		}
+		?>
         </div>
     </div>
 </div>
