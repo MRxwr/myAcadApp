bfc108ec6266d8c358ebd13a35a067de15022626
MRxwr <nasserhatab@gmail.com>
2023-08-22 23:07:03 +0300

    Update translate.php

--- admin/includes/translate.php
+++ admin/includes/translate.php
@@ -2,14 +2,14 @@
 $cookieSession = "myAcad";
 $settingsTitle = "MY ACAD";
 $settingslogo = "logo.png";
-if ( isset($_GET["lang"]) ){
+if ( isset($_GET["Lang"]) ){
 	$arrayLangs = ["ENG","AR"];
-	if ( in_array($_GET["lang"], $arrayLangs) ){
-		setcookie("CREATEkwLANG","{$_GET["lang"]}",(86400*30) + time(), "/");
-		header("Refresh:0 , url=" . str_replace("?lang={$_GET["lang"]}", "" ,str_replace("&lang={$_GET["lang"]}", "", $_SERVER['REQUEST_URI'])) );
+	if ( in_array($_GET["Lang"], $arrayLangs) ){
+		setcookie("CREATEkwLANG","{$_GET["Lang"]}",(86400*30) + time(), "/");
+		header("Refresh:0 , url=" . str_replace("?Lang={$_GET["Lang"]}", "" ,str_replace("&Lang={$_GET["Lang"]}", "", $_SERVER['REQUEST_URI'])) );
 	}else{
 		setcookie("CREATEkwLANG","ENG",(86400*30) + time(), "/");
-		header("Refresh:0 , url=" . str_replace("?lang=ENG", "" ,str_replace("&lang=ENG", "", $_SERVER['REQUEST_URI'])) );
+		header("Refresh:0 , url=" . str_replace("?Lang=ENG", "" ,str_replace("&Lang=ENG", "", $_SERVER['REQUEST_URI'])) );
 	}
 }elseif( !isset($_COOKIE["CREATEkwLANG"]) ){
 	$_COOKIE["CREATEkwLANG"] = "ENG";
