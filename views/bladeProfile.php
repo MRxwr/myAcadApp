c88a41114afc8d516bcd78c0b3f65f207f30ebd9
MRxwr <nasserhatab@gmail.com>
2023-08-25 22:03:49 +0300

    Update bladeProfile.php

--- views/bladeProfile.php
+++ views/bladeProfile.php
@@ -1,6 +1,8 @@
 <?php
 if ( getLoginStatusResponse() == 0 ){
     header("LOCATION: ?v=Login&error=2");
+}elseif( $user = selectDB("users","`keepMeAlive` LIKE '{$_COOKIE["createmyacad"]}'") ){
+
 }
 require_once("template/editProfileModal.php");
 ?>
@@ -8,12 +10,12 @@ require_once("template/editProfileModal.php");
 <div class="home_area">
     <div class="left_side">
         <div class="profile_area">
-            <h2>Bader Hussain</h2>
+            <h2><?php echo $user[0]["firstName"] . " " . $user[0]["lastName"] ?></h2>
             <ul>
-                <li><a href="#"><img src="img/img_5.svg" alt=""><p>Wallet Amount (<span>2 KD</span>)</p></a></li>
-                <li><a href="#profile" data-toggle="modal"><img src="img/img_6.svg" alt=""><p>Profile</p></a></li>
-                <li><a href="?v=Logout"><img src="img/img_7.svg" alt=""><p>Log Out</p></a></li>
-                <li><a href="?v=Delete"><img src="img/img_8.svg" alt=""><p>Delete My Account</p></a></li>
+                <li><a href="#"><img src="img/img_5.svg" alt=""><p>Wallet Amount (<span><?php echo $user[0]["wallet"] ?> KD</span>)</p></a></li>
+                <li><a href="#profile" data-toggle="modal"><img src="img/img_6.svg" alt=""><p><?php echo direction("Profile","الملف الشخصي") ?></p></a></li>
+                <li><a href="?v=Logout"><img src="img/img_7.svg" alt=""><p><?php echo direction("Log Out","تسجيل خروج") ?></p></a></li>
+                <li><a href="?v=Delete"><img src="img/img_8.svg" alt=""><p><?php echo direction("Delete my account","حذف حسابي") ?></p></a></li>
             </ul>
         </div>
     </div>
