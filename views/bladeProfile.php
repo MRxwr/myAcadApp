e3aebc8c4f4dc899e46990f94e65c17fde7248bd
MRxwr <nasserhatab@gmail.com>
2023-08-25 22:16:26 +0300

    Update bladeProfile.php

--- views/bladeProfile.php
+++ views/bladeProfile.php
@@ -12,7 +12,7 @@ require_once("template/editProfileModal.php");
         <div class="profile_area">
             <h2><?php echo $user[0]["firstName"] . " " . $user[0]["lastName"] ?></h2>
             <ul>
-                <li><a href="#"><img src="img/img_5.svg" alt=""><p>Wallet Amount (<span><?php echo $user[0]["wallet"] ?> KD</span>)</p></a></li>
+                <li><a href="#"><img src="img/img_5.svg" alt=""><p><?php echo direction("Wallet amount","قيمة المحفظة") ?> (<span><?php echo $user[0]["wallet"] ?> KD</span>)</p></a></li>
                 <li><a href="#profile" data-toggle="modal"><img src="img/img_6.svg" alt=""><p><?php echo direction("Profile","الملف الشخصي") ?></p></a></li>
                 <li><a href="?v=Logout"><img src="img/img_7.svg" alt=""><p><?php echo direction("Log Out","تسجيل خروج") ?></p></a></li>
                 <li><a href="?v=Delete"><img src="img/img_8.svg" alt=""><p><?php echo direction("Delete my account","حذف حسابي") ?></p></a></li>
