2ca1ea14eb3f18f1c575625288471f04bc25a417
MRxwr <nasserhatab@gmail.com>
2023-08-25 21:06:50 +0300

    Update bladeLogin.php

--- views/bladeLogin.php
+++ views/bladeLogin.php
@@ -7,6 +7,11 @@ if( isset($_POST["email"]) && !empty($_POST["email"]) ){
     }
     die();
 }
+if( isset($_GET["error"]) && !empty($_GET["error"]) ){
+    ?>
+    <script>alert('<?php echo direction("Wrong email/ password, Please try again!","بريد إلكتروني / كلمة مرور خاطئه ، الرجاء المحاولة مجدداً") ?>')</script>
+    <?php
+}
 ?>
 <div class="home_area">
     <div class="left_side">
@@ -17,17 +22,17 @@ if( isset($_POST["email"]) && !empty($_POST["email"]) ){
             <form action="?v=Login" method="post" class="wapper_form">
                 <div class="input_wapp">
                     <img src="img/img_1.svg" alt="">
-                    <input type="email" name="email" placeholder="Email Address">
+                    <input type="email" name="email" placeholder="<?php echo direction("Email Address", "البريد الإلكتروني") ?>">
                 </div>
                 <div class="input_wapp">
                     <img src="img/img_2.svg" alt="">
-                    <input type="password" name="password" placeholder="Password">
+                    <input type="password" name="password" placeholder="<?php echo direction("Password","كلمة المرور") ?>">
                 </div>
-                <button class="button" type="submit">LOGIN</button>
+                <button class="button" type="submit"><?php echo direction("LOGIN","تسجيل الدخول") ?></button>
             </form>
             <ul>
-                <li><a href="?v=Register">Register</a></li>
-                <li><a href="?v=ForgetPassword">Forget Password?</a></li>
+                <li><a href="?v=Register"><?php echo direction("Register","تسجيل حساب جديد") ?></a></li>
+                <li><a href="?v=ForgetPassword"><?php echo direction("Forget Password","نسيت كلمة المرور؟") ?></a></li>
             </ul>
         </div>
     </div>
