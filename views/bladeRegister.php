87cd0b928f0806c9d056a291ed5c239a6385bc1f
MRxwr <nasserhatab@gmail.com>
2023-08-26 11:56:02 +0300

    Update bladeRegister.php

--- views/bladeRegister.php
+++ views/bladeRegister.php
@@ -9,7 +9,7 @@ if( isset($_POST["firstName"]) && !empty($_POST["firstName"]) ){
       CURLOPT_TIMEOUT => 0,
       CURLOPT_FOLLOWLOCATION => true,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => '',
+      CURLOPT_CUSTOMREQUEST => 'POST',
       CURLOPT_POSTFIELDS => $_POST,
       CURLOPT_HTTPHEADER => array(
         'myacadheader: myAcadAppCreate'
