26a5a757eb91a4b7b7a1267cba04f950239b5daf
MRxwr <nasserhatab@gmail.com>
2023-08-26 10:03:58 +0300

    Update bladeRegister.php

--- views/bladeRegister.php
+++ views/bladeRegister.php
@@ -1,36 +1,36 @@
 <div class="home_area">
     <div class="left_side">
         <div class="hero_input sign_form">
-            <h2>SIGNUP</h2>
+            <h2><?php echo direction("SIGNUP","تسجيل") ?></h2>
             <form action="#" class="wapper_form">
                 <div class="input_wapp">
                     <img src="img/img_3.svg" alt="">
-                    <input type="text" placeholder="First Name">
+                    <input type="text" placeholder="<?php echo direction("First Name","الإسم الأول") ?>">
                 </div>
                 <div class="input_wapp">
                     <img src="img/img_3.svg" alt="">
-                    <input type="text" placeholder="Last Name">
+                    <input type="text" placeholder="<?php echo direction("Last Name","الإسم الأخير") ?>">
                 </div>
                 <div class="input_wapp">
                     <img src="img/img_1.svg" alt="">
-                    <input type="email" placeholder="Email Address">
+                    <input type="email" placeholder="<?php echo direction("Email Address","البريد الإلكتروني") ?>">
                 </div>
                 <div class="input_wapp bor_style">
                     <img src="img/img_4.svg" alt="">
-                    <input type="tel" id="phn" value="+965" placeholder="Phone Number">
+                    <input type="tel" id="phn" value="+965" placeholder="<?php echo direction("Phone Number","رقم الهاتف") ?>">
                 </div>
                 <div class="input_wapp">
                     <img src="img/img_2.svg" alt="">
-                    <input type="password" placeholder="Password">
+                    <input type="password" placeholder="<?php echo direction("Password","كلمة المرور") ?>">
                 </div>
                 <div class="input_wapp">
                     <img src="img/img_2.svg" alt="">
-                    <input type="password" placeholder="Re-Type Password">
+                    <input type="password" placeholder="<?php echo direction("Re-Type Password","تأكيد كلمة المرور") ?>">
                 </div>
-                <button class="button mt_40" type="submit">SIGNUP</button>
+                <button class="button mt_40" type="submit"><?php echo direction("SIGNUP","تسجيل") ?></button>
             </form>
             <ul>
-                <li><a href="?v=Login">Already have an account? Login.</a></li>
+                <li><a href="?v=Login"><?php echo direction("Already have an account? Login.","لديك حساب من قبل، سجل دخولك") ?></a></li>
             </ul>
         </div>
     </div>
