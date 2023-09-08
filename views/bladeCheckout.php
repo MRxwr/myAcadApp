05a733e7b84f6cf7face075fbb0ab678e64a0504
MRxwr <nasserhatab@gmail.com>
2023-09-09 00:22:45 +0300

    buidling checkout

--- views/bladeCheckout.php
+++ views/bladeCheckout.php
@@ -1,5 +1,47 @@
 <?php
-var_dump($_POST);
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://createkwservers.com/myacad1/requests?a=Academy&academyId={$_POST["checkout"]["id"]}",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'myacadheader: myAcadAppCreate'
+  ),
+));
+$response = curl_exec($curl);
+curl_close($curl);
+$response = json_decode($response,true);
+if( $response["error"] == 1 ){
+	?>
+	<script>
+	window.onload = function() {
+		alert("<?php echo direction("Erorr while loading academy data.","حدث خطأ اثناء تحميل بيانات الأكادمية.") ?>");
+		window.history.back();
+	};
+	</script>
+	<?php
+}else{
+	$academy = $response["data"]["academy"];
+}
+if( !isset($_POST["checkout"]["jersy"]) || empty($_POST["checkout"]["jersy"]) ){
+    $_POST["checkout"]["jersy"] = 0;
+    $jersyPrice = (float)$academy["clothesPrice"]*(float)$_POST["checkout"]["jersy"];
+}else{
+    $jersyPrice = 0;
+}
+
+if( $session = selectDB("sessions","`id` = '{$_POST["checkout"]["session"]}'")){}
+if( $subscription = selectDB("subscriptions","`id` = '{$_POST["checkout"]["subscription"]}'")){
+    $price = ($subscription[0]["priceAfterDiscount"] != 0 ) ? $subscription[0]["priceAfterDiscount"] : $subscription[0]["price"] ;
+    $totalPrice = (float)$price*(float)$_POST["checkout"]["quantity"];
+}
+if( $user = selectDB("users","`keepMeAlive` LIKE '{$_COOKIE["createmyacad"]}'") ){ }
+$checkout = json_decode($_POST,true);
 ?>
 <div class="checkout_area mt_20 pb_50">
     <div class="container">
@@ -8,47 +50,54 @@ var_dump($_POST);
                 <div class="row justify-content-between">
                     <div class="col-lg-5 order-lg-2 mt_40">
                         <div class="right_succes chek_right">
-                            <h3>ORDER INFO</h3>
-                            <h2>Dynamo Kuwait</h2>
+                            <h3><?php echo direction("ORDER INFO","معلومات الإشتراك") ?></h3>
+                            <h2><?php echo direction($academy["enTitle"],$academy["arTitle"]) ?></h2>
                             <div class="suc_item">
                                 <div class="suc_child">
-                                    <span>1</span>
-                                    <h3>2001 - 2005, Time</h3>
+                                    <span><?php echo $_POST["checkout"]["quantity"] ?></span>
+                                    <h3><?php echo direction($session[0]["enTitle"],$session[0]["arTitle"]) ?></h3>
                                 </div>
-                                <p>30 KD</p>
+                                <p><?php echo $totalPrice ?>KD</p>
                             </div>
+                            <?php 
+                            if ( !empty($_POST["checkout"]["jersy"]) ){
+                        ?>
                             <div class="suc_item">
                                 <div class="suc_child">
-                                    <span>1</span>
-                                    <h3>Dynamo Kuwait Jeresy</h3>
+                                    <span><?php echo $_POST["checkout"]["jersy"] ?></span>
+                                    <h3><?php echo direction($academy["enTitle"] . " Jersy","ملابس " . $academy["arTitle"]) ?></h3>
                                 </div>
-                                <p>15 KD</p>
+                                <p><?php echo $jersyPrice ?>KD</p>
                             </div>
+                        <?php
+                            }
+                            ?>
                         </div>
                     </div>
                     <div class="col-lg-5 order-lg-1 mt_40">
                         <div class="check_out">
-                            <h2>PAYMENT METHOD</h2>
-                            <form action="?v=Payment">
+                            <h2><?php echo direction("PAYMENT METHOD", "طرق الدفع" ) ?></h2>
+                            <form action="?v=Payment" method="post" >
                                 <div class="shape_items">
-                                    <input type="radio" checked="" name="out" id="out_1">
+                                    <input type="radio" checked="" name="payment" id="out_1">
                                     <label for="out_1"><span></span>Knet</label>
                                 </div>
                                 <div class="shape_items">
-                                    <input type="radio" name="out" id="out_2">
+                                    <input type="radio" name="payment" id="out_2">
                                     <label for="out_2"><span></span>Visa / Master Card</label>
                                 </div>
                                 <div class="shape_items">
-                                    <input type="radio" name="out" id="out_3">
-                                    <label for="out_3"><span></span>WALLET <p>  ( 20 KD )</p></label>
+                                    <input type="radio" name="payment" id="out_3">
+                                    <label for="out_3"><span></span><?php echo direction("Wallet","المحفظة") ?> <p>  ( <?php echo $user[0]["wallet"] ?>KD )</p></label>
                                 </div>
+                                <input type="hidden" name="data" value="<?php echo $checkout ?>">
+                                <div class="d-flex justify-content-between mt_50 extre_h6">
+                                    <h6><strong><?php echo direction("Total", "المجموع") ?></strong></h6>
+                                    <span><?php echo $jersyPrice + $totalPrice ?>KD</span>
+                                </div>
+                                <p>By clicking Pay Now, you agree to our <a href="#">Terms & Conditions</a></p>
+                                <button class="button"><?php echo direction("CHECKOUT","تابع للدفع") ?></button>
                             </form>
-                            <div class="d-flex justify-content-between mt_50 extre_h6">
-                                <h6><strong>Total</strong></h6>
-                                <span>45 KD</span>
-                            </div>
-                            <p>By clicking Pay Now, you agree to our <a href="#">Terms & Conditions</a></p>
-                            <a href="#" class="button">CHECKOUT</a>
                         </div>
                     </div>
                 </div>
