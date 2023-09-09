02d6b538d3a7fab6ccbdb9582243a88d8943d5c9
MRxwr <nasserhatab@gmail.com>
2023-09-09 23:40:09 +0300

    Update bladePayment.php

--- views/bladePayment.php
+++ views/bladePayment.php
@@ -29,6 +29,7 @@ if( isset($_POST["data"]) && !empty($_POST["data"]) ){
     $response = curl_exec($curl);
     curl_close($curl);
     $response = json_decode($response,true);
+    print_r($response);die();
     if( $response["error"] == 0 ){
         header("LOCATION: {$response["data"]["data"]["paymentURL"]}");
     }else{
