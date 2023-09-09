63ba17f65e9b1e74fe41af792537c89e07b58aff
MRxwr <nasserhatab@gmail.com>
2023-09-09 23:45:09 +0300

    Update apiPayment.php

--- requests/views/apiPayment.php
+++ requests/views/apiPayment.php
@@ -5,6 +5,7 @@ if( !isset($_POST) ){
 	echo outputError($response);die();
 }else{
     $data = $_POST;
+    print_r($data);
     unset($_POST);
     $user = $data["user"];
     $academy = $data["academy"];
