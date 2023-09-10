c680bd70b96289fce5f13fe91132232b5a3dc823
MRxwr <nasserhatab@gmail.com>
2023-09-11 01:21:59 +0300

    Update apiSubscriptions.php

--- requests/views/apiSubscriptions.php
+++ requests/views/apiSubscriptions.php
@@ -3,7 +3,7 @@ if( !isset($_GET["userId"]) || empty($_GET["userId"]) ){
 	$response = array("msg"=>"Please set user id");
 	echo outputError($response);die();
 }else{
-	if( $orders = selectDB("`id`,`date`,`academyId`","orders","`userId` = '{$_GET["userId"]}' AND `status` = '1'") ){
+	if( $orders = selectDB2("`id`,`date`,`academyId`","orders","`userId` = '{$_GET["userId"]}' AND `status` = '1'") ){
         for( $i = 0; $i < sizeof($orders); $i++ ){
             $academy = selectDB2("`enTitle`,`arTitle`,`imageurl`,`location`","academies","`id` = '{$orders[$i]["academyId"]}'");
             $sport = selectDB2("`imageurl`","sports","`id` = '{$academy[0]["sport"]}'");
