e883eda8440ab94c503d79703e0613a0fd6bdca1
MRxwr <nasserhatab@gmail.com>
2023-09-11 00:34:19 +0300

    Update bladeSuccess.php

--- views/bladeSuccess.php
+++ views/bladeSuccess.php
@@ -1,7 +1,7 @@
 <?php
 if( isset($_GET["OrderID"]) && !empty($_GET["OrderID"]) ){
     if( $order = selectDB("orders","`gatewayId` = '{$_GET["OrderID"]}'") ){
-        if( $order[0]["status"] != 0 ){
+        if( $order[0]["status"] == 0 ){
             updateDB("orders",array("gatewayLink"=>json_encode($_GET),"status"=>1),"`gatewayId` = '{$_GET["OrderID"]}'");
             $paymentMethod = (($order[0]["paymentMethod"] == 1 ) ? "Knet" : ($order[0]["paymentMethod"] == 2 )) ? "Visa" : "Cash";
             $subscription = selectDB("subscriptions","`id` = '{$order[0]["subscriptionId"]}'");
