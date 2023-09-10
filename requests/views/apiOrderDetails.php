0c3e4e3891c2cf00569d2dd83709ce1e449140e7
MRxwr <nasserhatab@gmail.com>
2023-09-11 01:04:24 +0300

    Update apiOrderDetails.php

--- requests/views/apiOrderDetails.php
+++ requests/views/apiOrderDetails.php
@@ -3,8 +3,8 @@ if( !isset($_POST["orderId"]) || empty($_POST["orderId"]) ){
 	$response = array("msg"=>"Please set order id");
 	echo outputError($response);die();
 }else{
-	if( $order = selectDB2("`id`, `date`, `paymentMethod`, `enAcademy`, `arAcademy`, `enSession`, `arSession`, `enSubscription`, `arSubscription`, `subscriptionQuantity`, `jersyQuantity`, `totalSubscriptionPrice`, `totalJersyPrice`, `total`","orders","`gatewayId` = '{$_POST["invoiceId"]}'") ){
-        $order2 = selectDB("orders","`gatewayId` = '{$_POST["invoiceId"]}'");
+	if( $order = selectDB2("`id`, `date`, `paymentMethod`, `enAcademy`, `arAcademy`, `enSession`, `arSession`, `enSubscription`, `arSubscription`, `subscriptionQuantity`, `jersyQuantity`, `totalSubscriptionPrice`, `totalJersyPrice`, `total`","orders","`gatewayId` = '{$_POST["orderId"]}'") ){
+        $order2 = selectDB("orders","`gatewayId` = '{$_POST["orderId"]}'");
         $subscription = selectDB("subscriptions","`id` = '{$order2[0]["subscriptionId"]}'");
         $order[0]["endDate"] = date("Y-m-d H:i:s", strtotime($order[0]["date"] . " +{$subscription[0]["numberOfDays"]} days"));
         $response = $order;
