5751cdfbc22c885a975fd7c9303d01bd56389130
MRxwr <nasserhatab@gmail.com>
2023-09-20 00:55:31 +0300

    Update bladeSubscriptions.php

--- views/bladeSubscriptions.php
+++ views/bladeSubscriptions.php
@@ -75,7 +75,6 @@ function mySubscriptions($type){
                                         <h4><?php echo direction("Invoice","الفاتورة") ?></h4>
                                     </a>
                                     <?php
-                                    echo date("Y-m-d H:i:s") . "<br>" . date("Y-m-d H:i:s", strtotime("+2 days", strtotime($result["data"][$i]["date"])));
                                     if( date("Y-m-d H:i:s") < date("Y-m-d H:i:s", strtotime("+2 days", strtotime($result["data"][$i]["date"]))) ){
                                         ?>
                                         <a href="#" class="item_sub">
