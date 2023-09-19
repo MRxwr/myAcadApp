8bbe3fc93f14344ee19ed76ef94db10e2e1fdafb
MRxwr <nasserhatab@gmail.com>
2023-09-20 00:27:30 +0300

    Update bladeSubscriptions.php

--- views/bladeSubscriptions.php
+++ views/bladeSubscriptions.php
@@ -17,7 +17,7 @@ function mySubscriptions($type){
     ));
     $response = curl_exec($curl);
     curl_close($curl);
-    return $response;
+    return json_decode($response,true);
 }
 ?>
 <div class="subsicription_area">
