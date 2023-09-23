0ad2b9f8a9b8c0c9508333772e8198ccc9cea1b1
Sonjoy Bhadra <sbhadra0@gmail.com>
2023-09-23 15:19:16 +0530

    Update bladeLogout.php

--- views/bladeLogout.php
+++ views/bladeLogout.php
@@ -1,5 +1,5 @@
-<?php
-setcookie("createmyacad", "logged out", time() - (86400*30 ), "/");
-header("LOCATION: ?v=Home");
+<?php setcookie("createmyacad", "logged out", time() - (86400*30 ), "/");
+//header("LOCATION: ?v=Home");
+echo '<script>window.location.href = "?v=Home";</script>';
 die();
 ?>
