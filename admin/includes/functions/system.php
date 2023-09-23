96faa303b5a81ca5e53c3dea38a74e6e4543d69f
Sonjoy Bhadra <sbhadra0@gmail.com>
2023-09-23 17:49:28 +0530

    Update system.php

--- admin/includes/functions/system.php
+++ admin/includes/functions/system.php
@@ -2,8 +2,8 @@
 // user \\
 function getLoginStatus(){
 	$output = "";
+	echo $_COOKIE["createmyacad"];
 	if( isset($_COOKIE["createmyacad"]) && !empty($_COOKIE["createmyacad"]) && $user = selectDB("users","`keepMeAlive` LIKE '{$_COOKIE["createmyacad"]}'") ){
-		
 		$output = "<a href='?v=Logout' class='button'>".direction("Logout","تسجيل الخروج")."</a>";
 	}else{
 		$output = "<a href='?v=Login' class='button'>".direction("Login","تسجيل الدخول")."</a>";
