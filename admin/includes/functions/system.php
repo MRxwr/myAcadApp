7e63abc986dde56409866bff386fa8e01c7c780e
Sonjoy Bhadra <sbhadra0@gmail.com>
2023-09-23 18:10:01 +0530

    Update system.php

--- admin/includes/functions/system.php
+++ admin/includes/functions/system.php
@@ -28,6 +28,8 @@ function userLogin($data){
 	if ( $user = selectDB("users","`email` LIKE '{$data["email"]}' AND `password` LIKE '".sha1($data["password"])."' AND `status` = '0' AND `hidden` = '0'") ){
 		$randomCookie = sha1(rand(000000,999999)+time());
 		setcookie("createmyacad", $randomCookie, time() + (86400*30 ), "/");
+        echo $_COOKIE["createmyacad"];
+		var_dump($user); exit;
 		if( updateDB("users",array('keepMeAlive' => $randomCookie),"`id` = '{$user[0]["id"]}'") ){
 			return 1;
 		}else{
