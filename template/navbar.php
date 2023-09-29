729ef742bb0e92e6906951e52e585c3dd89d764f
MRxwr <nasserhatab@gmail.com>
2023-09-29 17:49:49 +0300

    Update navbar.php

--- template/navbar.php
+++ template/navbar.php
@@ -6,6 +6,7 @@ if( isset($_GET["country"]) && !empty($_GET["country"]) ){
     setcookie("createmyacadcountry", "KW", time() + (86400*30 ), "/");
     $_COOKIE["createmyacadcountry"] = "KW";
 }
+var_dump($_COOKIE);
 ?>
 
 <header>
