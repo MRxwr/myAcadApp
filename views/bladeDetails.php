54bc3890b86dd41d77132db7244fbb62e3b3de13
MRxwr <nasserhatab@gmail.com>
2023-10-05 01:37:18 +0300

    Update bladeDetails.php

--- views/bladeDetails.php
+++ views/bladeDetails.php
@@ -127,7 +127,7 @@ $redirect = ($academy["isClothes"] == 1 ) ? "Jersy" : "Checkout" ;
             <div class="col-lg-10">
                 <h2><img src="img/map.svg" alt=""><?php echo direction("Location","الموقع") ?></h2>
                 <a href="<?php echo $academy["location"] ?>">
-                    <img src="<?php echo "logos/{$academy["locationImage"]}" ?>" style="" alt="">
+                    <img src="<?php echo "logos/{$academy["locationImage"]}" ?>" style="width: 100%;height: 250px;" alt="">
                 </a>
             </div>
         </div>
