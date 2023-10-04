b5d5ade71049ea8b47920dc5a9e14007aa0752c2
MRxwr <nasserhatab@gmail.com>
2023-10-05 01:36:06 +0300

    Update apiAcademy.php

--- requests/views/apiAcademy.php
+++ requests/views/apiAcademy.php
@@ -3,7 +3,7 @@ if( !isset($_GET["academyId"]) || empty($_GET["academyId"]) ){
 	$response = array("msg"=>"Please set academy id");
 	echo outputError($response);die();
 }else{
-	if( $academy = selectDB2("`id`, `imageurl`, `enTitle`, `arTitle`, `area`, `video`, `location`, `isClothes`, `clothesPrice`, `clothesImage`","academies","`hidden` = '0' AND `status` = '0' AND `id` = {$_GET["academyId"]}") ){
+	if( $academy = selectDB2("`id`, `imageurl`, `enTitle`, `arTitle`, `area`, `video`, `location`, `isClothes`, `clothesPrice`, `clothesImage`, `locationImage`","academies","`hidden` = '0' AND `status` = '0' AND `id` = {$_GET["academyId"]}") ){
 		$response["academy"] = $academy[0];
 		if( $area = selectDB("countries","`id` = '{$academy[0]["area"]}'") ){
 			$response["academy"]["enArea"] = $area[0]["areaEnTitle"];
