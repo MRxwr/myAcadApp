c0f80c9d404c5b80c98f0a8fd443866023508873
MRxwr <nasserhatab@gmail.com>
2023-10-08 01:07:45 +0300

    Update apiUser.php

--- requests/views/apiUser.php
+++ requests/views/apiUser.php
@@ -59,7 +59,7 @@ if ( isset($_GET["type"]) && !empty($_GET["type"]) ){
 			  CURLOPT_POSTFIELDS => array(
 				'site' => '- MyAcad',
 			  	'subject' => 'New Password - MyAcad',
-			  	'body' => "<img src='../img/logo.png' style='width:200px;height:200px'>
+			  	'body' => "<img src='https://createkwservers.com/myacad1/img/logo.png' style='width:200px;height:200px'>
 				  <p>&nbsp;</p>Your new password is: {$random}<br><br>(Note: Please change your passowrd as soon as you login in app.)",
 			  	'from_email' => 'noreply@myacad.com',
 			  	'to_email' => $_GET["email"]),
