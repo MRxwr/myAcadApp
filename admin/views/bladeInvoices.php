22c142c08702df59c64a15073e81b1cf79dc514e
MRxwr <nasserhatab@gmail.com>
2023-10-08 01:26:42 +0300

    Create bladeInvoices.php

---
+++ admin/views/bladeInvoices.php
@@ -0,0 +1,55 @@
+<div class="col-sm-12">
+<div class="panel panel-default card-view">
+<div class="panel-heading">
+<div class="pull-left">
+<h6 class="panel-title txt-dark"><?php echo direction("List of Invoices", "قائمة الفواتير") ?></h6>
+</div>
+<div class="clearfix"></div>
+</div>
+<div class="panel-wrapper collapse in">
+<div class="panel-body">
+<div class="table-wrap mt-40">
+<div class="table-responsive">
+
+	<table class="table display responsive product-overview mb-30" id="myTable">
+		<thead>
+		<tr>
+		<th>#</th>
+		<th><?php echo direction("Date","التاريخ") ?></th>
+		<th><?php echo direction("Name","الإسم") ?></th>
+		<th><?php echo direction("Mobile","الهاتف") ?></th>
+		<th><?php echo direction("Academy","الأكادميه") ?></th>
+		<th><?php echo direction("Total","المجموع") ?></th>
+		<th><?php echo direction("Status","الحالة") ?></th>
+		<th class="text-nowrap"><?php echo direction("Actions","الخيارات") ?></th>
+		</tr>
+		</thead>
+		
+		<tbody>
+		<?php 
+		if( $orders = selectDB("orders","`id` != '0' ORDER BY `date` DESC") ){
+            for( $i = 0; $i < sizeof($orders); $i++ ){
+            ?>
+                <tr>
+                <td><?php echo $orders[$i]["id"] ?></td>
+                <td><?php echo substr($orders[$i]["date"],0,11) ?></td>
+                <td><?php echo $orders[$i]["name"] ?></td>
+                <td><?php echo $orders[$i]["phone"] ?></td>
+                <td><?php echo direction($orders[$i]["enAcademy"],$orders[$i]["arAcademy"]) ?></td>
+                <td><?php echo $orders[$i]["total"] ?>KD</td>
+                <td><?php echo $orders[$i]["status"] ?></td>
+                <td class="text-nowrap"></td>
+                </tr>
+            <?php
+            }
+		}
+		?>
+		</tbody>
+	</table>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
