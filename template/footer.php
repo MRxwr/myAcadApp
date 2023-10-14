e3679d87d548314276d405b192586055b45f78ee
MRxwr <nasserhatab@gmail.com>
2023-10-14 15:34:25 +0300

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -58,9 +58,9 @@
     <a href="#" class="back-to-top"><i class="fal fa-angle-up"></i></a>
     
     <!-- all js here -->
-    <script src="js/jquery-3.4.1.min.js"></script>
-    <script src="js/plugins.js"></script>
-    <script src="js/main.js"></script>
+    <script src="js/jquery-3.4.1.min.js?<?php echo randLetter() . "=" . rand(0000,9999) ?>"></script>
+    <script src="js/plugins.js?<?php echo randLetter() . "=" . rand(0000,9999) ?>"></script>
+    <script src="js/main.js?<?php echo randLetter() . "=" . rand(0000,9999) ?>"></script>
     <script>
         $(document).on("click","input[type=radio]",function(){
             var id = $(this).attr("id");
