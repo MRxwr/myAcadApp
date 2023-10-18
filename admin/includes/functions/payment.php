a12c72b8727d5fd8ab36fce0c54c718ad04cc2d0
MRxwr <nasserhatab@gmail.com>
2023-10-18 17:25:25 +0300

    update payment

--- admin/includes/functions/payment.php
+++ admin/includes/functions/payment.php
@@ -170,7 +170,7 @@ function checkProductDiscountDefault($id){
 function payment($data){
 	$curl = curl_init();
 	curl_setopt_array($curl, array(
-	  CURLOPT_URL => 'https://createkwservers.com/payapi/api/v2/index.php',
+	  CURLOPT_URL => 'https://createapi.link/api/v2/index.php',
 	  CURLOPT_RETURNTRANSFER => true,
 	  CURLOPT_ENCODING => '',
 	  CURLOPT_MAXREDIRS => 10,
@@ -198,7 +198,7 @@ function payment($data){
 function checkPayment($data){
 	$curl = curl_init();
 	curl_setopt_array($curl, array(
-	  CURLOPT_URL => 'https://createkwservers.com/payapi/api/v2/index.php',
+	  CURLOPT_URL => 'https://createapi.link/api/v2/index.php',
 	  CURLOPT_RETURNTRANSFER => true,
 	  CURLOPT_ENCODING => '',
 	  CURLOPT_MAXREDIRS => 10,
@@ -265,7 +265,7 @@ function getInternationalShipping($items,$address){
 	);
 	$curl = curl_init();
 	curl_setopt_array($curl, array(
-	CURLOPT_URL => 'https://createkwservers.com/payapi/api/v2/index.php',
+	CURLOPT_URL => 'https://createapi.link/api/v2/index.php',
 	CURLOPT_RETURNTRANSFER => true,
 	CURLOPT_ENCODING => '',
 	CURLOPT_MAXREDIRS => 10,
