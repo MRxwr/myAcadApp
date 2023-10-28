857300b78a2dd50ad1cd69829121ce66739dc1c9
MRxwr <nasserhatab@gmail.com>
2023-10-29 01:21:41 +0300

    Update apiPayment.php

--- requests/views/apiPayment.php
+++ requests/views/apiPayment.php
@@ -130,7 +130,7 @@ if( !isset($_POST) ){
 	curl_setopt($ch, CURLOPT_POSTFIELDS,$fields_string);
 	// receive server response ...
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$server_output = curl_exec($ch);
+	echo $server_output = curl_exec($ch);
 	curl_close ($ch);
 	$response = json_decode($server_output,true);
 
