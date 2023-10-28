d97efcc313766d51eeb30e6c32b12cc1d2668270
MRxwr <nasserhatab@gmail.com>
2023-10-28 15:59:38 +0300

    Update bannersSlider.php

--- template/bannersSlider.php
+++ template/bannersSlider.php
@@ -4,10 +4,11 @@
 		<?php
 		if( $banners = selectDB("banners","`status` = '0' AND `hidden` = '0' ORDER BY `order` ASC") ){
 			for( $i = 0; $i < sizeof($banners) ; $i++ ){
+				$banners[$i]["link"] = (!empty($banners[$i]["link"]) || $banners[$i]["link"] != "#" ) ? $banners[$i]["link"] : "?v=Home" ;
 				if( $banners[$i]["type"] == 0 ){
 					echo "<div class='item'><a href='{$banners[$i]["link"]}' target='_blank' alt='Link-{$banners[$i]["title"]}'><img src='logos/{$banners[$i]["imageurl"]}' alt='{$banners[$i]["title"]}'></a></div>";
 				}else{
-					echo "<div class='item'><div class='play_video' style='background-image: url(logos/{$banners[$i]["imageurl"]});background-size: cover;'><a href='{$banners[$i]["link"]}' class='watch_btn'><i class='fal fa-play-circle'></i></a></div>";
+					echo "<div class='item'><div class='play_video' style='background-image: url(logos/{$banners[$i]["imageurl"]});background-size: fill;'><a href='{$banners[$i]["link"]}' class='watch_btn'><i class='fal fa-play-circle'></i></a></div>";
 				}
 			}
 		}
