ac448d37187e425dc6f568b203241503983d20c6
MRxwr <nasserhatab@gmail.com>
2023-10-28 20:47:51 +0300

    Update bladeDetails.php

--- views/bladeDetails.php
+++ views/bladeDetails.php
@@ -103,7 +103,7 @@ $redirect = ($academy["isClothes"] == 1 ) ? "Jersy" : "Checkout" ;
 											<input type='radio' {$checked} name='session' id='sty_{$i}' value='{$academy["sessions"][$i]["id"]}'>
 											<label for='sty_{$i}'><span></span>".direction($academy["sessions"][$i]["enTitle"],$academy["sessions"][$i]["arTitle"])."</label>
 										</div>
-										<input type='number' step='1' class='sty_{$i}' name='quantity[]' value='{$number}'  min='0' readonly>
+										<input type='number' step='1' class='sty_{$i}' name='quantity[]' value='{$number}'  min='0' max='{$academy["sessions"][$i]["quantity"]}' readonly>
 									</div>
 									";
 								}
