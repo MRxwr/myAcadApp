6b77582c69409b60067c85f9844e74603fcabfc5
MRxwr <nasserhatab@gmail.com>
2023-11-01 00:03:40 +0300

    Update bladeReports.php

--- admin/views/bladeReports.php
+++ admin/views/bladeReports.php
@@ -2,7 +2,7 @@
 <div class="panel panel-default card-view">
 <div class="panel-heading">
 <div class="pull-left">
-	<?php echo direction("Report Details","تفاصيل التقرير") ?>
+	<h6 class="panel-title txt-dark"><?php echo direction("Report Details","تفاصيل التقرير") ?></h6>
 </div>
 </div>
 <div class="panel-wrapper collapse in">
