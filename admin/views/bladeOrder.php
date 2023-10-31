a48ce4f8969533c6c85e050a70f4d5a2558a0d92
MRxwr <nasserhatab@gmail.com>
2023-10-31 17:52:28 +0300

    Update bladeOrder.php

--- admin/views/bladeOrder.php
+++ admin/views/bladeOrder.php
@@ -1,5 +1,15 @@
 <?php
 
-echo "hello";
+if( $order = selectDB("orders","`orderId` = '{$_GET["id"]}'") ){
+
+}else{
+    ?>
+    <script>
+        alert("<?php echo direction("Wrong order number","رقم طلب خاطئ") ?>");
+        window.location.href = "?v=Invoices";
+        
+    </script>
+    <?php
+}
 
 ?>
