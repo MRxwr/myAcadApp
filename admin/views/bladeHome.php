40289541469fb20c427bf51b8a403df0f38eede8
MRxwr <nasserhatab@gmail.com>
2023-11-04 01:39:23 +0300

    Update bladeHome.php

--- admin/views/bladeHome.php
+++ admin/views/bladeHome.php
@@ -58,7 +58,7 @@
 }
 </style>
 <div class="row" style="padding:16px">
-	<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
 			<div class="panel panel-default card-view">
 			<div class="panel-heading">
 				<div class="pull-left">
