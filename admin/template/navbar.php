9b90d59087c01963a17d4805c3e98fd1f5cd81a4
MRxwr <nasserhatab@gmail.com>
2023-11-04 01:49:46 +0300

    Update navbar.php

--- admin/template/navbar.php
+++ admin/template/navbar.php
@@ -21,7 +21,7 @@
 							</li>
 						<?php
 						$languageText = direction("العربية","English");
-						$languageParam = direction("Lang=AR","Lang=ENG");
+						$languageParam = direction("Lang=AR","Lang=EN");
 						?>
 							<li>
 								<a href="<?php echo $_SERVER['REQUEST_URI'] . getSign() . $languageParam ?>"><i class="fa fa-language"></i><span><?php echo $languageText ?></span></a>
