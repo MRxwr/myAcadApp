a19e9eacb6cd72859abada2510e6500e34fa97c1
MRxwr <nasserhatab@gmail.com>
2023-11-04 01:52:06 +0300

    Update footer.php

--- admin/template/footer.php
+++ admin/template/footer.php
@@ -9,7 +9,7 @@
 <footer class="footer container-fluid pl-30 pr-30">
 	<div class="row">
 		<div class="col-sm-12">
-			<p>2020 &copy; Create-KW - E-Commerce System</p>
+			<p>2023 &copy; Createkuwait.com - MYACAD Control Panel</p>
 		</div>
 	</div>
 </footer>
