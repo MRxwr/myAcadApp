a0f3e14c57ec22d22760de3c00bfe1121e56620e
MRxwr <nasserhatab@gmail.com>
2023-11-04 00:37:43 +0300

    Update bladeHome.php

--- admin/views/bladeHome.php
+++ admin/views/bladeHome.php
@@ -173,7 +173,7 @@ for ( $y =0; $y < 3; $y++){
 	}
 }
 ?>		
-	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 		<div class="panel panel-default card-view">
 		<div class="panel-wrapper collapse in">
 		<div class="panel-body row">
@@ -184,10 +184,10 @@ for ( $y =0; $y < 3; $y++){
 		</label>
 		<thead>
 		<tr>
-		<th><?php echo $DateTime ?></th>
-		<th><?php echo $OrderID ?></th>
-		<th><?php echo $Price ?></th>
-		<th><?php echo $Actions ?></th>
+		<th><?php echo direction("Date","التاريخ") ?></th>
+		<th><?php echo direction("#","#") ?></th>
+		<th><?php echo direction("Price","السعر") ?></th>
+		<th><?php echo direction("Actions","الخيارات") ?></th>
 		</tr>
 		</thead>
 		<tbody>
