306ad2f73a3f6e26612bc48d9471d46e3a81ccd2
MRxwr <nasserhatab@gmail.com>
2023-11-05 11:52:25 +0300

    Update apiSettings.php

--- requests/views/apiSettings.php
+++ requests/views/apiSettings.php
@@ -1,13 +1,13 @@
 <?php 
 if( $settings = selectDB2("`version`, `enTerms`, `arTerms`, `enPolicy`, `arPolicy`","settings","`id` = '1'" ) ){
 	if( $social = selectDB2("`youtube`,`whatsapp`, `instagram`, `tiktok`, `snapchat`, `location`, `email`","social_media","`id` = '1'") ){
-		$response["social"][] = "https://youtube.com/{$social[0]["youtube"]}";
-		$response["social"][] = "https://wa.me/{$social[0]["whatsapp"]}";
-		$response["social"][] = "https://instagram.com/{$social[0]["instagram"]}";
-		$response["social"][] = $social[0]["tiktok"];
-		$response["social"][] = $social[0]["snapchat"];
-		$response["social"][] = $social[0]["location"];
-		$response["social"][] = $social[0]["email"];
+		$response["social"]["youtube"] = "https://youtube.com/{$social[0]["youtube"]}";
+		$response["social"]["whatsapp"] = "https://wa.me/{$social[0]["whatsapp"]}";
+		$response["social"]["instagram"] = "https://instagram.com/{$social[0]["instagram"]}";
+		$response["social"]["tiktok"] = $social[0]["tiktok"];
+		$response["social"]["snapchat"] = $social[0]["snapchat"];
+		$response["social"]["location"] = $social[0]["location"];
+		$response["social"]["email"] = $social[0]["email"];
 	}
 	$response["settings"] = $settings[0];
 	echo outputData($response);
