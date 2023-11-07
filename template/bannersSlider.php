98284db0bd4a089438a7ad1b3222707838d6397e
MRxwr <nasserhatab@gmail.com>
2023-11-07 23:55:14 +0300

    Update bannersSlider.php

--- template/bannersSlider.php
+++ template/bannersSlider.php
@@ -6,9 +6,9 @@
 			for( $i = 0; $i < sizeof($banners) ; $i++ ){
 				$banners[$i]["link"] = ( empty($banners[$i]["link"]) || $banners[$i]["link"] == "#" ) ? "?v=Home" : $banners[$i]["link"] ;
 				if( $banners[$i]["type"] == 0 ){
-					echo "<div class='item'><a href='{$banners[$i]["link"]}' target='_blank' alt='Link-{$banners[$i]["title"]}'><img src='logos/{$banners[$i]["imageurl"]}' alt='{$banners[$i]["title"]}'></a></div>";
+					echo "<div class='item'><a href='{$banners[$i]["link"]}' target='_blank' alt='Link-{$banners[$i]["title"]}'><img src='logos/{$banners[$i]["imageurl600"]}' alt='{$banners[$i]["title"]}'></a></div>";
 				}else{
-					echo "<div class='item'><div class='play_video' style='background-image: url(logos/{$banners[$i]["imageurl"]});background-size: fill;'><a href='{$banners[$i]["link"]}' class='watch_btn'><i class='fal fa-play-circle'></i></a></div>";
+					echo "<div class='item'><div class='play_video' style='background-image: url(logos/{$banners[$i]["imageurl600"]});background-size: fill;'><a href='{$banners[$i]["link"]}' class='watch_btn'><i class='fal fa-play-circle'></i></a></div>";
 				}
 			}
 		}
