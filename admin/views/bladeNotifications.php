a5b2477c293e0f714f1b75d9d2d2cfd76895f6f5
MRxwr <nasserhatab@gmail.com>
2023-12-13 01:03:50 +0300

    Update bladeNotifications.php

--- admin/views/bladeNotifications.php
+++ admin/views/bladeNotifications.php
@@ -8,7 +8,7 @@ if ( isset($_POST["firebaseTitle"]) ){
                 "message" => $_POST["firebaseMsg"],
                 "firebase" => $users["firebase"]
             );
-            sendNotification($data);
+            print_r(sendNotification($data));
         }
     }
 }
