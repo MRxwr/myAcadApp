82a8afa296a7992eb5a16da60cc01ffd23f76bef
MRxwr <nasserhatab@gmail.com>
2023-12-13 02:28:56 +0300

    Create apiGenders.php

---
+++ requests/views/apiGenders.php
@@ -0,0 +1,13 @@
+<?php 
+if( $academies = selectDB2("`gender`","academies","`sport` = '{$_GET["sportId"]}' AND `hidden` = '0' AND `status` = '0'") ){
+    $gendersEn = ["SELECT GENDER","Man","Woman","Boy","Girl"];
+    $gendersAr = ["إختيار الجنس","رجل","إمرأة","ولد","بنت"];
+    for( $i = 0; $i < sizeof($academies); $i++ ){
+        $response["genders"][] = array("genderEn" => $gendersEn[$academies[$i]["gender"]], "genderAr" => $gendersAr[$academies[$i]["gender"]]);
+    }
+}else{
+    $response["genders"] = array();
+    echo outputError($response);die();
+}
+echo outputData($response);
+?>
