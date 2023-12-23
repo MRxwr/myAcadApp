ca8fdcd44a508e7c1d2b1f98ed81de3131d119dc
MRxwr <nasserhatab@gmail.com>
2023-12-24 00:41:45 +0300

    Update leftSideBar.php

--- admin/template/leftSideBar.php
+++ admin/template/leftSideBar.php
@@ -16,9 +16,8 @@ if( $pages = selectDB("pages","`status` = '0' AND `section` = '0' ORDER BY `orde
 			}
 		}
 		if( $listOfpages = selectDB("`pages`","`enTitle` LIKE '{$_GET["v"]}' AND `id` IN ({$listOfAllowedPages})") ){
-			if( ($jsonPages = json_decode($listOfpages,true)) == null && isset($jsonPages["ok"]) && $jsonPages["ok"] == "false" ){
-				header("LOCATION: ?v=Home");die();
-			}
+		}else{
+			header("LOCATION: ?v=Home");die();
 		}
 	}else{
 		$list = array();
