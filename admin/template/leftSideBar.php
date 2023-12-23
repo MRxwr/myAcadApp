eaa00c519177894dc5810c15dc4f73d80e46e21f
MRxwr <nasserhatab@gmail.com>
2023-12-24 00:37:08 +0300

    Update leftSideBar.php

--- admin/template/leftSideBar.php
+++ admin/template/leftSideBar.php
@@ -17,7 +17,7 @@ if( $pages = selectDB("pages","`status` = '0' AND `section` = '0' ORDER BY `orde
 		}
 		if( $listOfpages = selectDB("`pages`","`enTitle` LIKE '{$_GET["v"]}' AND `id` IN ({$listOfAllowedPages})") ){
 			if( isset($listOfpages["ok"]) && $listOfpages["ok"] == "false" ){
-				print_r($listOfpages); //header("LOCATION: ?v=Home");die();
+				header("LOCATION: ?v=Home");die();
 			}
 		}
 	}else{
