becb19b300234e37b0a7cd9b3b7edb48dc9b323e
MRxwr <nasserhatab@gmail.com>
2024-01-01 03:13:46 +0300

    Update apiSubscriptions.php

--- requests/views/apiSubscriptions.php
+++ requests/views/apiSubscriptions.php
@@ -12,7 +12,7 @@ if( !isset($_GET["userId"]) || empty($_GET["userId"]) ){
     }else{
         $_GET["type"] = $_GET["type"];
     }
-    if ($orders = selectDB("orders", "`userId` = '{$_GET["userId"]}' AND `status` != '4'")) {
+    if ($orders = selectDB("orders", "`userId` = '{$_GET["userId"]}' AND `status` = '1'")) {
         for ($i = 0; $i < sizeof($orders); $i++) {
             $subscriptions = selectDB("subscriptions", "`id` = '{$orders[$i]["subscriptionId"]}'");
             $numberOfDays = $subscriptions[0]["numberOfDays"];
