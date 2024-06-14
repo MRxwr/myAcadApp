006b6332e5e05eb4660fef1d5d43603dc44a3506
MRxwr <nasserhatab@gmail.com>
2024-06-15 01:29:09 +0300

    Update apiPayment.php

--- requestsTest/views/apiPayment.php
+++ requestsTest/views/apiPayment.php
@@ -211,24 +211,24 @@ if( !isset($_POST) ){
 
     $response = curl_exec($curl);
     curl_close($curl);
-    var_dump($response);
+
     $response = json_decode($response,true);
     //saving info and redirecting to payment pages
-    if( isset($response["data"]["link"]) && !empty($response["data"]["link"]) ){
-        $_POST["gatewayId"] = $response["data"]["trackId"];
-        $_POST["gatewayURL"] = $response["data"]["link"];
+    if( isset($response["status"]) && $response["status"] == "successful" && isset($response["data"]["paymentURL"]) && !empty($response["data"]["paymentURL"]) ){
+        $_POST["gatewayId"] = $response["data"]["InvoiceId"];
+        $_POST["gatewayURL"] = $response["data"]["paymentURL"];
         $_POST["apiPayload"] = json_encode($postBody);
         $_POST["apiResponse"] = json_encode($response);
         $_POST["paymentMethod"] = ( $wallet == 1 ) ? 3 : $paymentMethod;
         $response["data"] = array(
-            "paymentURL" => $response["data"]["link"],
-            "InvoiceId"  => $response["data"]["trackId"]
+            "paymentURL" => $response["data"]["paymentURL"],
+            "InvoiceId"  => $response["data"]["InvoiceId"]
         );
         insertDB2("orders",$_POST);
         if( $wallet == 1 ){
             $array["data"] = array(
                 "paymentURL" => "index.php?v=Success&OrderID={$_POST["gatewayId"]}&Result=CAPTURED",
-                "InvoiceId" => $response["data"]["trackId"]
+                "InvoiceId" => $response["data"]["InvoiceId"]
             );
             if( $user = selectDB("users","`id` = {$_POST["userId"]}") ){
                 $newWallet = $user[0]["wallet"] - $_POST["total"];
