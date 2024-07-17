01a9961f1b9de52a0c77f19fd04d2622067cf1d2
MRxwr <nasserhatab@gmail.com>
2024-07-17 21:36:07 +0300

    Update apiOrder.php

--- requestsTest/views/apiOrder.php
+++ requestsTest/views/apiOrder.php
@@ -12,18 +12,20 @@ if( !isset($_POST["invoiceId"]) || empty($_POST["invoiceId"]) ){
 	if( $order = selectDB2("`id`, `date`, `paymentMethod`, `enAcademy`, `arAcademy`, `enSession`, `arSession`, `enSubscription`, `arSubscription`, `subscriptionQuantity`, `jersyQuantity`, `totalSubscriptionPrice`, `totalJersyPrice`,`voucher`, `total`","orders","`gatewayId` = '{$_POST["invoiceId"]}'") ){
         $order2 = selectDB("orders","`gatewayId` = '{$_POST["invoiceId"]}'");
         if( $order2[0]["status"] == 0 ){
-            updateDB("orders",array("gatewayLink"=>json_encode($_POST["url"]),"status"=>1),"`gatewayId` = '{$_POST["invoiceId"]}'");
-            $session = selectDB("sessions","`id` = '{$order2[0]["sessionId"]}'");
-            $quantity = $session[0]["quantity"] - $order2[0]["subscriptionQuantity"];
-            updateDB("sessions",array("quantity"=>$quantity),"`id` = '{$order2[0]["sessionId"]}'");
-            $subscription = selectDB("subscriptions","`id` = '{$order2[0]["subscriptionId"]}'");
-            $order[0]["endDate"] = date("Y-m-d H:i:s", strtotime($order[0]["date"] . " +{$subscription[0]["numberOfDays"]} days"));
+            updateDB("orders",array("gatewayLink"=>json_encode($_POST["url"]),"status"=>$_POST["status"]),"`gatewayId` = '{$_POST["invoiceId"]}'");
+            if( $_POST["status"] == 1 ){
+                $session = selectDB("sessions","`id` = '{$order2[0]["sessionId"]}'");
+                $quantity = $session[0]["quantity"] - $order2[0]["subscriptionQuantity"];
+                updateDB("sessions",array("quantity"=>$quantity),"`id` = '{$order2[0]["sessionId"]}'");
+                $subscription = selectDB("subscriptions","`id` = '{$order2[0]["subscriptionId"]}'");
+                $order[0]["endDate"] = date("Y-m-d H:i:s", strtotime($order[0]["date"] . " +{$subscription[0]["numberOfDays"]} days"));
+                $academyEmail = selectDB("academies","`id` = '{$order2[0]["academyId"]}'");
+                $settingsEmail = selectDB("settings","`id` = '1'");
+                sendMails($order2,$order2[0]["email"]);
+                sendMails($order2,$academyEmail[0]["email"]);
+                sendMails($order2,$settingsEmail[0]["email"]);
+            }
             $response = $order;
-            $academyEmail = selectDB("academies","`id` = '{$order2[0]["academyId"]}'");
-            $settingsEmail = selectDB("settings","`id` = '1'");
-            sendMails($order2,$order2[0]["email"]);
-            sendMails($order2,$academyEmail[0]["email"]);
-            sendMails($order2,$settingsEmail[0]["email"]);
         }else{
             $response = $order;
         }
