ef4902729c83ad67e189e2006bd1baeefbbd75e1
MRxwr <nasserhatab@gmail.com>
2024-08-06 00:38:46 +0300

    Update payment.php

--- payment.php
+++ payment.php
@@ -50,7 +50,7 @@ if( isset($_POST["data"]) && !empty($_POST["data"]) ){
     ?>
 	<script>
 	window.onload = function() {
-		alert("<?php echo direction("Erorr while loading payment data.","حدث خطأ اثناء تحميل بيانات الدفع.") ?>");
+		alert("<?php echo direction("Erorr while loading post data payment data.","حدث خطأ اثناء تحميل بيانات الدفع.") ?>");
 		window.location.href = "?v=Home";
 	};
 	</script>
