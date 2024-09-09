6914e815c09c736f3411985eebcce542af353993
MRxwr <nasserhatab@gmail.com>
2024-09-09 23:51:43 +0300

    save

--- dev/requests/views/apiPurchases.php
+++ dev/requests/views/apiPurchases.php
@@ -21,59 +21,56 @@ if( !isset($_POST) ){
             }
         }
     }
-    
-    //preparing upayment payload
-    $extraMerchantData =  array(
-        'amounts' => array($fullAmount),
-        'charges' => array(0.250),
-        'chargeType' => array('fixed'),
-        'cc_charges' => array(0.250),
-        'cc_chargeType' => array('fixed'),
-        'ibans' => array("{$iban}")
-    );
-    $comon_array = array(
-        "merchant_id"=> "24072",
-        "username"=> "create_lwt",
-        "password"=> stripslashes('sJg@Q9N6ysvP'),
-        "api_key"=> password_hash('afmceR6nHQaIehhpOel036LBhC8hihuB8iNh9ACF',PASSWORD_BCRYPT),
-        "payment_gateway" => "knet",
-        "order_id"=> time(),
-        'total_price'=>$fullAmount,
-        'success_url'=>'https://myacad.app/Purchase.php',
-        'error_url'=>'https://myacad.app/Purchase.php',
-        'notifyURL'=>'https://myacad.app/Purchase.php',
-        'test_mode'=>0,
-        "whitelabled" => 1,
-        'CurrencyCode'=>'KWD',			
-        'CstFName'=>"PURCHASE MYACAD",	
-        'Cstemail'=>"{$AdminSettings[0]["email"]}",
-        'CstMobile'=>"00{$socialMedia[0]["whatsapp"]}",
-        'ExtraMerchantsData'=> json_encode($extraMerchantData),
-        'ProductTitle' => "Checkout List",
-        'ProductName' => json_encode(array( 0 => "Purchase {$title}")),
-        'ProductPrice' => json_encode(array( 0 => $fullAmount)),
-        'ProductQty' => json_encode(array( 0 => 1)),
-    );
+    $orderId = time();
+    $postBody = array(
+        'language' => 'en',
+        'paymentGateway[src]' => "knet",
+        'order[id]' => $orderId,
+        'order[currency]' => 'KWD',
+        'order[amount]' => (string)$fullAmount,
+        'order[description]' => "Checkout List for 1x " . json_encode(array( 0 => "Purchase {$title}")),
+        'reference[id]' => $orderId,
+        'customer[name]' => "PURCHASE MYACAD",
+        'customer[email]' => "{$AdminSettings[0]["email"]}",
+        'customer[mobile]' => "00{$socialMedia[0]["whatsapp"]}",
+        'returnUrl' => 'https://myacad.app/Purchase.php',
+        'cancelUrl' => 'https://myacad.app/Purchase.php',
+        'notificationUrl' => 'https://myacad.app/Purchase.php',
+        'extraMerchantData[0][amount]' => (string)$fullAmount,
+        'extraMerchantData[0][knetCharge]' => '0.25',
+        'extraMerchantData[0][knetChargeType]' => 'fixed',
+        'extraMerchantData[0][ccCharge]' => '0.25',
+        'extraMerchantData[0][ccChargeType]' => 'fixed',
+        'extraMerchantData[0][ibanNumber]' => "{$iban}",
+        );
 
-    $fields_string = http_build_query($comon_array);
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_URL,"https://api.upayments.com/payment-request");
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS,$fields_string);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$server_output = curl_exec($ch);
-	curl_close ($ch);
-	$response = json_decode($server_output,true);
+	$curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://uapi.upayments.com/api/v1/charge',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => $postBody,
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer afmceR6nHQaIehhpOel036LBhC8hihuB8iNh9ACF',
+        ),
+    ));
+    $response = curl_exec($curl);
+    curl_close($curl);
+    $response = json_decode($response,true);
 
-    if( $response["status"] == "success" && isset($response["paymentURL"]) && !empty($response["paymentURL"]) ){
-        $_POST["gatewayId"] = $comon_array["order_id"];
-        $_POST["gatewayURL"] = $response["paymentURL"];
-        $_POST["apiPayload"] = json_encode($comon_array);
+    if( $response["status"] == "true" && isset($response["data"]["link"]) && !empty($response["data"]["link"]) ){
+        $_POST["gatewayId"] = $orderId;
+        $_POST["gatewayURL"] = $response["data"]["link"];
+        $_POST["apiPayload"] = json_encode($postBody);
         $_POST["apiResponse"] = json_encode($response);
         $response["data"] = array(
-            "paymentURL" => $response["paymentURL"],
-            "InvoiceId"  => $comon_array["order_id"]
+            "paymentURL" => $response["data"]["link"],
+            "InvoiceId"  => $orderId
         );
         updateDB("purchases",$_POST,"`id` = '{$data["id"]}'");
         echo outputData($response);
