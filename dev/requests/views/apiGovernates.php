7b429c4067b5236498532075e003c873c22c30d3
MRxwr <nasserhatab@gmail.com>
2024-09-15 17:07:21 +0300

    Create apiGovernates.php

---
+++ dev/requests/views/apiGovernates.php
@@ -0,0 +1,39 @@
+<?php 
+if( !isset($_GET["sportId"]) || empty($_GET["sportId"]) ){
+    $response["error"] = array(
+        "msg" => "sportId is required"
+    );
+    echo json_encode($response);die();
+}
+if( !isset($_GET["countryCode"]) || empty($_GET["countryCode"]) ){
+    $response["data"] = array(
+        "msg" => "countryCode  is required"
+    );
+    echo json_encode($response);die();
+}
+if( !isset($_GET["genderId"]) || empty($_GET["genderId"]) ){
+    $response["data"] = array(
+        "msg" => "genderId  is required"
+    );
+    echo json_encode($response);die();
+}
+if( $academies = selectDB2("`gender`","academies","`sport` = '{$_GET["sportId"]}' AND `country` LIKE '{$_GET["countryCode"]}' AND `gender` = '{$_GET["genderId"]}' AND `hidden` = '0' AND `status` = '0' GROUP BY `governate`") ){
+    $response["governates"][0] = array(
+        "id" => 0,
+        "enGovernates" => "",
+        "arGovernates" => ""
+    );
+    for( $i = 0; $i < sizeof($academies); $i++ ){
+        $governate = selectDB("governates","`id` = '{$academies[$i]["governate"]}'");
+        $response["genders"][] = array(
+            "id" => $governate[0]["id"],
+            "enGovernate" => $governate[0]["enTitle"],
+            "arGovernate" => $governate[0]["arTitle"]
+        );
+    }
+}else{
+    $response["governates"] = array();
+    echo outputError($response);die();
+}
+echo outputData($response);
+?>
