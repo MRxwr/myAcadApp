b0097c393d4258a14d0dd64e6383ef7abd99cb25
MRxwr <nasserhatab@gmail.com>
2024-09-15 17:42:59 +0300

    Update footer.php

--- dev/template/footer.php
+++ dev/template/footer.php
@@ -58,11 +58,11 @@
     <a href="#" class="back-to-top"><i class="fal fa-angle-up"></i></a>
     
     <!-- all js here -->
-    <script src="js/jquery-3.4.1.min.js?<?php echo randLetter() . "=" . rand(0000,9999) ?>"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
-    <script src="js/plugins.js?<?php echo randLetter() . "=" . rand(0000,9999) ?>"></script>
-    <script src="js/main.js?<?php echo randLetter() . "=" . rand(0000,9999) ?>"></script>
-    <script>
+    <script src="js/jquery-3.4.1.min.js?<?php echo randLetter() . "=" . rand(0000,9999) ?>" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js" defer></script>
+    <script src="js/plugins.js?<?php echo randLetter() . "=" . rand(0000,9999) ?>" defer></script>
+    <script src="js/main.js?<?php echo randLetter() . "=" . rand(0000,9999) ?>" defer></script>
+    <script defer>
         $(document).on("click","#voucherBtn", function(){
           var voucher = $("#appliedVoucher").val();
           var total = $("#total").val();
