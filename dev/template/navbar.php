d29065373b51a5752c2a66c35e48adb7ebc38b6a
MRxwr <nasserhatab@gmail.com>
2024-09-15 15:10:52 +0300

    Update navbar.php

--- dev/template/navbar.php
+++ dev/template/navbar.php
@@ -1,10 +1,9 @@
 <?php
+if( !isset($_COOKIE["createmyacadcountry"]) ){
+    setcookie("createmyacadcountry", "KW", time() + (86400*30 ), "/");
+}
 if( isset($_GET["country"]) && !empty($_GET["country"]) ){
     setcookie("createmyacadcountry", $_GET["country"], time() + (86400*30 ), "/");
-    $_COOKIE["createmyacadcountry"] = $_GET["country"];
-}else{
-    setcookie("createmyacadcountry", "KW", time() + (86400*30 ), "/");
-    $_COOKIE["createmyacadcountry"] = "KW";
 }
 ?>
 
