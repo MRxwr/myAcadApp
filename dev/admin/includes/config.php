3cb54c9b24de0ed770892b7a47334784030bfcd1
MRxwr <nasserhatab@gmail.com>
2024-09-15 14:30:21 +0300

    save

--- dev/admin/includes/config.php
+++ dev/admin/includes/config.php
@@ -3,6 +3,7 @@ $servername = "localhost";
 $username = "u800348178_myacad2USER";
 $password = "N@b$90949089";
 $dbname = "u800348178_myacadv2DB";
+$baseURL = "https://dev.myacad.app/requests";
 $dbconnect = new MySQLi($servername,$username,$password,$dbname);
 if ( $dbconnect->connect_error ){
 	die("Connection Failed: " .$dbconnect->connect_error );
