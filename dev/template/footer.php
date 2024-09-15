4a6c73b14e3c49b5f3b999dc277b976ace781136
MRxwr <nasserhatab@gmail.com>
2024-09-15 17:44:39 +0300

    Update footer.php

--- dev/template/footer.php
+++ dev/template/footer.php
@@ -61,7 +61,7 @@
     <script src="js/jquery-3.4.1.min.js?<?php echo randLetter() . "=" . rand(0000,9999) ?>"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
     <script src="js/plugins.js?<?php echo randLetter() . "=" . rand(0000,9999) ?>"></script>
-    <script src="js/main.js?<?php echo randLetter() . "=" . rand(0000,9999) ?>" defer></script>
+    <script src="js/main.js?<?php echo randLetter() . "=" . rand(0000,9999) ?>"></script>
     <script>
         $(document).on("click","#voucherBtn", function(){
           var voucher = $("#appliedVoucher").val();
