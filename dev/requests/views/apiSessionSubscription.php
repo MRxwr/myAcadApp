238d0323e68c1ce0f0c9bfdc29268e72f935a694
MRxwr <nasserhatab@gmail.com>
2024-09-24 01:16:58 +0300

    Create apiSessionSubscription.php

---
+++ dev/requests/views/apiSessionSubscription.php
@@ -0,0 +1,18 @@
+<?php 
+if( !isset($_GET["sessionId"]) || empty($_GET["sessionId"]) ){
+	$response = array("msg"=>"Please set session id");
+	echo outputError($response);die();
+}else{
+	if( $sessionSubscription = selectDB("session_subscription","`hidden` = '0' AND `status` = '0' AND `id` = {$_GET["sessionId"]}") ){
+        for ($i=0; $i < count($sessionSubscription); $i++) { 
+            $subscriptions = selectDB2("`id`, `enTitle`, `arTitle`, `price`, `priceAfterDiscount`","subscriptions","`id` = '{$sessionSubscription[$i]["id"]}' AND `status` = '0' AND `hidden` = '0' ORDER BY `price` ASC");
+            $response["subscriptions"][] = $subscriptions;
+        }
+	}else{
+		$response["msg"] = popupMsg($requestLang,"There is no subscriptions for this session","لا يوجد أشتراكات لهذا المحاضرة");
+		echo outputError($response);die();
+	}
+}
+
+echo outputData($response);
+?>
