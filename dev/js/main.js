7ba4a883259cc80b9f0c45842a9df23fe545c7ea
MRxwr <nasserhatab@gmail.com>
2024-09-24 17:35:22 +0300

    Update main.js

--- dev/js/main.js
+++ dev/js/main.js
@@ -276,7 +276,7 @@
                     value: item.id,
                     text: item[selectedLanguage]
                   });
-                  console.log($item);
+                  console.log(item);
                   $select.append($option);
                 });
                 $select.niceSelect('update');
