c62709a8d5690f501e0f7b502f2314e05706ad2b
MRxwr <nasserhatab@gmail.com>
2024-09-25 04:28:10 +0300

    Create apiSearchTournament.php

---
+++ dev/requests/views/apiSearchTournament.php
@@ -0,0 +1,44 @@
+<?php 
+if( !isset($_GET["sportId"]) || empty($_GET["sportId"]) ){
+	$response = array("msg"=>"Please set sport id");
+	echo outputError($response);die();
+}else{
+	$where = " AND `sport` = '{$_GET["sportId"]}'";
+	if( isset($_GET["genderId"]) && !empty($_GET["genderId"]) ){
+		$where .= " AND `gender` = '{$_GET["genderId"]}'";
+	}
+	if( isset($_GET["governateId"]) && !empty($_GET["governateId"]) ){
+		$where .= " AND `governate` = '{$_GET["governateId"]}'";
+	}
+	if( isset($_GET["areaId"]) && !empty($_GET["areaId"]) ){
+		$where .= " AND `area` = '{$_GET["areaId"]}'";
+	}
+	if( isset($_GET["keyword"]) && !empty($_GET["keyword"]) ){
+		$where .= " AND ( `enTitle` LIKE '%".$_GET["keyword"]."%' OR `arTitle` LIKE '%".$_GET["keyword"]."%')";
+	}
+	if( isset($_GET["countryCode"]) && !empty($_GET["countryCode"]) ){
+		$where .= " AND `country` = '{$_GET["countryCode"]}'";
+	}else{
+		$where .= " AND `country` = 'KW'";
+	}
+	if( $tournaments = selectDB2("`id`, `imageurl`, `header`, `enTitle`, `arTitle`, `area`, `price`, `isIndoor`","tournaments","`hidden` = '0' AND `status` = '0' {$where}") ){
+		for( $i = 0; $i < sizeof($tournaments); $i++){
+			$response["tournaments"][$i] = $tournaments[$i];
+			if( $area = selectDB("countries","`id` = '{$tournaments[$i]["area"]}'") ){
+				$response["tournaments"][$i]["enArea"] = $area[0]["areaEnTitle"];
+				$response["tournaments"][$i]["arArea"] = $area[0]["areaArTitle"];
+			}else{
+				$response["tournaments"][$i]["enArea"] = "";
+				$response["tournaments"][$i]["arArea"] = "";
+			}
+			$response["tournaments"][$i]["rating"] = 0;
+		}
+	}else{
+		$response["msg"] = popupMsg($requestLang,"No tournaments found","لا يوجد بطولات");
+		$response["tournaments"] = array();
+		echo outputError($response);die();
+	}
+}
+
+echo outputData($response);
+?>
