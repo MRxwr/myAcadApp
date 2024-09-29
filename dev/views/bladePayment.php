ac1be6e135634a2673c1469f57313e2367ca1605
MRxwr <nasserhatab@gmail.com>
2024-09-30 02:46:43 +0300

    Update bladePayment.php

--- dev/views/bladePayment.php
+++ dev/views/bladePayment.php
@@ -40,7 +40,7 @@ if( isset($_POST) && !empty($_POST) ){
         'myacadheader: myAcadAppCreate'
       ),
     ));
-    $response = curl_exec($curl);
+    echo $response = curl_exec($curl);
     curl_close($curl);
     $response = json_decode($response,true);
     if( $response["error"] == 0 ){
