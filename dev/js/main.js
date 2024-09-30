f3f1e27e93ac88ab3bba14de70bb6116fc6c8dbc
MRxwr <nasserhatab@gmail.com>
2024-09-30 13:15:06 +0300

    Update main.js

--- dev/js/main.js
+++ dev/js/main.js
@@ -168,7 +168,7 @@
             });
         
             // Check teamName field
-            var teamName = $("input[name='teamName[]']").val();
+            var teamName = $("input[name='teamName']").val();
             if (teamName === "") {
                 isValid = false;
             }
