fa1411b85a81a511265a5465f2037960268283d5
MRxwr <nasserhatab@gmail.com>
2024-09-30 13:33:09 +0300

    Update apiCheckTeamName.php

--- dev/requests/views/apiCheckTeamName.php
+++ dev/requests/views/apiCheckTeamName.php
@@ -1,5 +1,5 @@
 <?php 
-if( $teams = selectDBNew("orders",[$_GET["tournamentId"],$_GET["teamName"]],"`status` = '1' AND `tournamentId` = ? AND `teamDetails` LIKE CONCAT('%',?,'%')","") ){
+if( $teams = selectDBNew("orders",[$_GET["tournamentId"],$_GET["teamName"]],"`status` = '1' AND `tournamentId` = ? AND `teamName` LIKE ?","") ){
     echo outputError(array("msg"=>"Team name already exists."));
 }else{
     echo outputData(array("msg"=>"You can use this team name."));
