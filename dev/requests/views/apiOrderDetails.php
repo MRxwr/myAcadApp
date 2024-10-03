d61f72f7850afedc465b4d5a18fde75456827870
MRxwr <nasserhatab@gmail.com>
2024-10-04 02:51:47 +0300

    Update apiOrderDetails.php

--- dev/requests/views/apiOrderDetails.php
+++ dev/requests/views/apiOrderDetails.php
@@ -6,17 +6,22 @@ if( !isset($_POST["orderId"]) || empty($_POST["orderId"]) ){
 	if( $order = selectDB2("`id`, `date`, `isTournament`, `paymentMethod`, `enAcademy`, `arAcademy`, `enSession`, `arSession`, `enSubscription`, `arSubscription`, `subscriptionQuantity`, `jersyQuantity`, `totalSubscriptionPrice`, `totalJersyPrice`, `voucher`, `total`, `tournamentId`, `teamDetails`","orders","`id` = '{$_POST["orderId"]}'") ){
         if( $order[0]["isTournament"] == 1 ){
             $order[0]["teamDetails"] = json_decode($order[0]["teamDetails"],true);
+            $area = selectDB("countries","`id` = '{$tournament[0]["area"]}'");
             $tournaments = selectDB("tournaments","`id` = '{$order[0]["tournamentId"]}'");
-            $data[0]["teamDetails"]["enTournament"] = $tournaments[0]["enTitle"];
-            $data[0]["teamDetails"]["arTournament"] = $tournaments[0]["arTitle"];
-            $data[0]["teamDetails"]["teamName"] = $order[0]["teamDetails"]["teamName"];
-            $data[0]["teamDetails"]["players"] = $order[0]["teamDetails"]["players"];
-            $data[0]["teamDetails"]["bench"] = $order[0]["teamDetails"]["bench"];
-            $data[0]["teamDetails"]["quantity"] = $order[0]["teamDetails"]["quantity"];
-            $data[0]["teamDetails"]["price"] = $order[0]["teamDetails"]["price"];
-            $data[0]["teamDetails"]["total"] = $order[0]["teamDetails"]["total"];
-            $data[0]["total"] = $order[0]["teamDetails"]["total"];
-            $response = $data;
+            $data["id"] = $order[0]["id"];
+            $data["date"] = $order[0]["date"];
+            $data["enTournament"] = $tournaments[0]["enTitle"];
+            $data["arTournament"] = $tournaments[0]["arTitle"];
+            $data["areaEnTitle"] = $area[0]["areaEnTitle"];
+            $data["areaArTitle"] = $area[0]["areaArTitle"];
+            $data["gameDate"] = $tournaments[0]["gameDate"];
+            $data["gameTime"] = $tournaments[0]["gameTime"];
+            $data["teamName"] = $order[0]["teamDetails"]["teamName"];
+            $data["players"] = $order[0]["teamDetails"]["players"];
+            $data["bench"] = $order[0]["teamDetails"]["bench"];
+            $data["quantity"] = $order[0]["teamDetails"]["quantity"];
+            $data["price"] = $order[0]["teamDetails"]["price"];
+            $data["total"] = $order[0]["teamDetails"]["total"];            $response = $data;
         }else{
             unset($order[0]["tournamentId"],$order[0]["teamDetails"]);
             $order2 = selectDB("orders","`id` = '{$_POST["orderId"]}'");
