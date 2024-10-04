da299cc1b4f9806ba1a4b2f6014e34ed2b9b3564
MRxwr <nasserhatab@gmail.com>
2024-10-04 23:38:34 +0300

    Update bladeSubscriptions.php

--- dev/admin/views/bladeSubscriptions.php
+++ dev/admin/views/bladeSubscriptions.php
@@ -9,6 +9,7 @@ for( $z = 0; $z < $count; $z++ ){
 	}
 }
 $id .= ( isset($academiesList[0]) && !empty($academiesList[0]) ) ? "AND `academyId` IN ($listOfAcademies)" : "";
+echo "`status` = '0' {$id}";
 if( $order = selectDB("subscriptions","`status` = '0' {$id}") ){
 }else{
 	?>
