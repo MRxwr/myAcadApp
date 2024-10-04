f20c2300c3c6e7d52d524bda35ed34dd2296008d
MRxwr <nasserhatab@gmail.com>
2024-10-04 23:52:48 +0300

    Update index.php

--- dev/admin/index.php
+++ dev/admin/index.php
@@ -164,6 +164,7 @@ if ( isset($_GET["hide"]) || isset($_GET["show"]) || isset($_GET["delId"]) || is
 		}
 	}
 	if( isset($_GET["code"]) && !empty($_GET["code"]) ){
+        var_dump(!in_array($userType, $allowedEmpolyees));
         if( !in_array($userType, $allowedEmpolyees) ){
             if( !in_array($_GET["code"],$academiesList) ){
                 ?>
