208d84506856394a55e58e4eabc5a6188d140425
MRxwr <nasserhatab@gmail.com>
2024-10-05 01:45:22 +0300

    Update config.php

--- dev/admin/includes/config.php
+++ dev/admin/includes/config.php
@@ -1,10 +1,10 @@
 <?php
 $servername = "localhost";
-$username = "u800348178_myacad2USER";
+$usernameDB = "u800348178_myacad2USER";
 $password = "N@b$90949089";
 $dbname = "u800348178_myacadv2DB";
 $baseURL = "https://dev.myacad.app/requests";
-$dbconnect = new MySQLi($servername,$username,$password,$dbname);
+$dbconnect = new MySQLi($servername,$usernameDB,$password,$dbname);
 if ( $dbconnect->connect_error ){
 	die("Connection Failed: " .$dbconnect->connect_error );
 }
