ca77f869f83ee834afe992e9444ad58b53d3cdfa
MRxwr <nasserhatab@gmail.com>
2024-10-04 04:36:35 +0300

    Create apiPoints.php

---
+++ dev/requests/views/apiPoints.php
@@ -0,0 +1,18 @@
+<?php 
+if( $user = selectDB2("`points`,`wallet`","users","`id` = '{$_GET["userId"]}' " ) ){
+    $settings = selectDB("settings","`id` = 1");
+    $points = $user[0]["points"];
+    if( $settings[0]["pointsRedeemMin"] > $points ){
+        $error["msg"] = popupMsg($requestLang,"Not enough points to redeem","ليس لديك نقاط كافية لتحويلها");
+        echo outputError($error);die();
+    }else{
+        $wallet = $points + $user[0]["wallet"];
+        updateDB("users",array("wallet"=>$wallet,"points"=>0),"`id` = '{$_GET["userId"]}'");
+        $error["msg"] = popupMsg($requestLang,"Points transferred successfully","تم تحويل النقاط بنجاح");
+	    echo outputData($error);die();
+    }
+}else{
+	$error["msg"] = popupMsg($requestLang,"No user with this id","لا يوجد مستخدم بهذا الرقم");
+	echo outputError($error);die();
+}
+?>
