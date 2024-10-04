609ce369e27907cb78b0d7ffa5087cb87edd0b3c
MRxwr <nasserhatab@gmail.com>
2024-10-05 00:02:37 +0300

    Create bladeLogs.php

---
+++ dev/admin/views/bladeLogs.php
@@ -0,0 +1,67 @@
+<div class="col-sm-12">
+<div class="panel panel-default card-view">
+<div class="panel-heading">
+<div class="pull-left">
+<h6 class="panel-title txt-dark"><?php echo direction("List of Logs", "قائمة السجلات") ?></h6>
+</div>
+<div class="clearfix"></div>
+</div>
+<div class="panel-wrapper collapse in">
+<div class="panel-body">
+<div class="table-wrap">
+<div class="table-responsive">
+
+	<table class="table display responsive product-overview mb-30" id="myTable">
+		<thead>
+		<tr>
+		<th>#</th>
+		<th><?php echo direction("Date","التاريخ") ?></th>
+		<th><?php echo direction("Username","اسم المستخدم") ?></th>
+		<th><?php echo direction("Module","الوحدة") ?></th>
+		<th><?php echo direction("Action","العملية") ?></th>
+		<th><?php echo direction("Data","البيانات") ?></th>
+		</tr>
+		</thead>
+		
+		<tbody>
+		<?php 
+            if( $logs = selectDB("logs","`id` != '0' ") ){
+                for( $i = 0; $i < sizeof($logs); $i++ ){
+                    $status = [direction("Pending","إنتظار"),direction("Successful","ناجحه"),direction("Failed","فاشلة"),direction("Cancelled","ملغية"),direction("Ended","إنتهى")];
+                    $statusColor = ["default","success","info","danger","warning"];
+                    for( $y = 0; $y < sizeof($status); $y++ ){
+                        if( $logs[$i]["status"] == $y ){
+                            $orderStatus = $status[$y];
+                            $orderBtnColor = $statusColor[$y];
+                        }
+                    }
+                ?>
+                    <tr>
+                    <td><?php echo sprintf("%05d", $logs[$i]["id"]) ?></td>
+                    <td><?php echo $logs[$i]["date"] ?></td>
+                    <td><?php echo $logs[$i]["username"] ?></td>
+                    <td><?php echo $logs[$i]["module"] ?></td>
+                    <td><?php echo $logs[$i]["action"] ?></td>
+                    <td>
+                        <?php
+                        // $logs[$i]["sqlQuery"] has json encoded data i want to decode them and reveal each in new line
+                        $data = json_decode($logs[$i]["data"]);
+                        foreach($data as $key => $value){
+                            echo "<pre><code>$key : $value</code></pre>";
+                        }
+                        ?>
+                    </td>
+                    </tr>
+                <?php
+                }
+            }
+		?>
+		</tbody>
+	</table>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
