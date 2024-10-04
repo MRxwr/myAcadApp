42b32ec49445300eb0e2fda1c003a333a1de9662
MRxwr <nasserhatab@gmail.com>
2024-10-04 05:48:06 +0300

    Update bladeVouchers.php

--- dev/admin/views/bladeVouchers.php
+++ dev/admin/views/bladeVouchers.php
@@ -118,7 +118,7 @@
 				}
 
 				$type = ( $vouchers[$i]["type"] == 0 ) ? direction("Percentage","نسبة مؤوية") : direction("Fixed","قيمة ثابته") ;
-				var_dump($vouchers[$i]["academyId"]);
+				$vouchers[$i]["academyId"] = json_decode($vouchers[$i]["academyId"], true);
 				for( $j = 0; $j < sizeof($vouchers[$i]["academyId"]); $j++ ){
 					if( $academy = selectDB("academies","`id` = '{$vouchers[$i]["academyId"][$j]}'") ){
 						$academy = direction($academy[0]["enTitle"],$academy[0]["arTitle"]) . " - ";
