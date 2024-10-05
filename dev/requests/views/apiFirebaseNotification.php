0cea1f260744fa9a866083350a9da6d21161c9ea
MRxwr <nasserhatab@gmail.com>
2024-10-05 04:25:05 +0300

    Update apiFirebaseNotification.php

--- dev/requests/views/apiFirebaseNotification.php
+++ dev/requests/views/apiFirebaseNotification.php
@@ -13,6 +13,7 @@ function getAccessToken() {
     CURLOPT_POSTFIELDS => array('firebase_json'=> new CURLFILE('../../myacademy-bd81b-firebase-adminsdk-mdflj-3fbac4549d.json')),
     ));
     $response = curl_exec($curl);
+    var_dump($response);die();
     $response = json_decode($response, true);
     curl_close($curl);
     return $response["data"]['access_token'];
