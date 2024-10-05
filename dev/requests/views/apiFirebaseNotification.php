0a99744cad0a7a7d9d125a92f5351db5ed34c56c
MRxwr <nasserhatab@gmail.com>
2024-10-05 04:01:41 +0300

    save

---
+++ dev/requests/views/apiFirebaseNotification.php
@@ -0,0 +1,58 @@
+<?php
+require_once("../../vendor/autoload.php");
+use Google\Client;
+
+function getAccessToken() {
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://createapi.link/api/v1/request_token',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => array('firebase_json'=> new CURLFILE('../../myacademy-bd81b-firebase-adminsdk-mdflj-3fbac4549d.json')),
+    ));
+    $response = curl_exec($curl);
+    $response = json_decode($response, true);
+    curl_close($curl);
+    return $response["data"]['access_token'];
+}
+
+$bearer = getAccessToken();
+$notificationData = array(
+    "message" => array(
+        "notification" => array( 
+            "title" => "{$_POST["title"]}",
+            "body"  => "{$_POST["body"]}",
+            //"image" => "{$_POST["image"]}",
+        )
+    )
+);
+
+if( $users = selectDB("users", "`id` != '0' GROUP BY `firebase` ORDER BY `id` ASC") ){
+    for( $i = 0; $i < sizeof($users); $i++){
+        $notificationData["message"]["token"] = $users[$i]["firebase"];
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => 'https://fcm.googleapis.com/v1/projects/points-a1a14/messages:send',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'POST',
+          CURLOPT_POSTFIELDS => json_encode($notificationData),
+          CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $bearer
+          ),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+    }
+}
+?>
