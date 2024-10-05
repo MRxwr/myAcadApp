aa4e629b0ccdbb04052f91d0ee9755ac8b5284d1
MRxwr <nasserhatab@gmail.com>
2024-10-05 04:28:18 +0300

    Update apiFirebaseNotification.php

--- dev/requests/views/apiFirebaseNotification.php
+++ dev/requests/views/apiFirebaseNotification.php
@@ -13,7 +13,7 @@ function getAccessToken() {
     CURLOPT_POSTFIELDS => array('firebase_json'=> new CURLFILE('../../myacademy-bd81b-firebase-adminsdk-mdflj-3fbac4549d.json')),
     ));
     $response = curl_exec($curl);
-    var_dump($response);die();
+    var_dump("../../myacademy-bd81b-firebase-adminsdk-mdflj-3fbac4549d.json");die();
     $response = json_decode($response, true);
     curl_close($curl);
     return $response["data"]['access_token'];
@@ -37,7 +37,7 @@ if( $users = selectDB("users", "`id` = '500' GROUP BY `firebase` ORDER BY `id` A
         $notificationData["message"]["token"] = $users[$i]["firebase"];
         $curl = curl_init();
         curl_setopt_array($curl, array(
-          CURLOPT_URL => 'https://fcm.googleapis.com/v1/projects/points-a1a14/messages:send',
+          CURLOPT_URL => 'https://fcm.googleapis.com/v1/projects/myacademy-bd81b/messages:send',
           CURLOPT_RETURNTRANSFER => true,
           CURLOPT_ENCODING => '',
           CURLOPT_MAXREDIRS => 10,
