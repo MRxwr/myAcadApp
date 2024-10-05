9ba00eb601361a944d5c84a61d95d98ddd923ff2
MRxwr <nasserhatab@gmail.com>
2024-10-05 03:50:42 +0300

    Update bladeAcademies.php

--- dev/admin/views/bladeAcademies.php
+++ dev/admin/views/bladeAcademies.php
@@ -2,7 +2,7 @@
 <div class="panel panel-default card-view">
 <div class="panel-heading">
 <div class="pull-left">
-	<h6 class="panel-title txt-dark"><?php echo direction("Academy Details","تفاصيل الأكادمية") ?></h6>
+	<h6 class="panel-title txt-dark"><?php echo direction("Academy Details","تفاصيل الأكاديمية") ?></h6>
 </div>
 	<div class="clearfix"></div>
 </div>
