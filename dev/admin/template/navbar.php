ae15b173f26141966fe26773263aed91eaa74fd4
MRxwr <nasserhatab@gmail.com>
2024-10-16 21:49:02 +0300

    Update navbar.php

--- dev/admin/template/navbar.php
+++ dev/admin/template/navbar.php
@@ -11,7 +11,7 @@
 				<a id="toggle_nav_btn" class="toggle-left-nav-btn inline-block ml-20 pull-left" href="javascript:void(0);"><i class="zmdi zmdi-menu"></i></a>
 				<a id="toggle_mobile_nav" class="mobile-only-view" href="javascript:void(0);"><i class="zmdi zmdi-more"></i></a>
 			</div>
-			<div id="mobile_only_nav" class="mobile-only-nav pull-right">
+			<div id="mobile_only_nav" class="mobile-only-nav pull-right" style="background: #012169;">
 				<ul class="nav navbar-right top-nav pull-right">
 					<li class="dropdown auth-drp">
 						<a href="#" class="dropdown-toggle pr-0" data-toggle="dropdown"><img src="../img/logo.png" alt="user_auth" class="user-auth-img img-circle"/><span class="user-online-status"></span></a>
