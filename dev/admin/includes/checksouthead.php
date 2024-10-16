9708063208dd92609d93728769d00225a2580007
MRxwr <nasserhatab@gmail.com>
2024-10-17 00:04:11 +0300

    Update checksouthead.php

--- dev/admin/includes/checksouthead.php
+++ dev/admin/includes/checksouthead.php
@@ -10,7 +10,7 @@ if ( isset($_COOKIE[$cookieSession."A"]) && !empty($_COOKIE[$cookieSession."A"])
 		$userID = $user[0]["id"];
 		$email = $user[0]["email"];
 		$username = $user[0]["fullName"];
-		$userType = $user[0]["empType"];
+		$userType = ( $user[0]["empType"] == 8 || $user[0]["empType"] == 16 ) ? 0 : $user[0]["empType"];
 		$academiesList = ( is_array($user[0]["academyId"]) && empty($user[0]["academyId"]) ) ? array() : json_decode($user[0]["academyId"],true);
 		$_SESSION[$cookieSession."A"] = $email;	
 	}else{
