873dec76630aa6ebb667eac99e9741ecdba98a2b
MRxwr <nasserhatab@gmail.com>
2024-10-17 02:27:36 +0300

    Update selectSportModal.php

--- dev/template/selectSportModal.php
+++ dev/template/selectSportModal.php
@@ -30,3 +30,37 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="sport2" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered sport_modal">
+        <div class="modal-content">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <div class="modal-body text-center">
+                <h2><?php echo direction("SELECT SPORT","إختر الرياضة") ?></h2>
+                <div class="extra_scr" data-simplebar>
+                    <div class="row">
+					<?php
+					if( $tournament = selectDB("{$table}","`country` LIKE '{$_GET["countryCode"]}' AND `hidden` = '0' AND `status` = '0' GROUP BY `sport` ORDER BY `sport` ASC") ){
+						for( $i = 0; $i < sizeof($sports); $i++){
+                            $sports = selectDB("sports","`id` = '{$tournament[$i]["sport"]}'");
+					?>
+                        <div class="col-lg-3 col-sm-4 col-4 mt_30">
+                            <a href="#" id="<?php echo $sports[$i]["id"] ?>" class="selectSport">
+                                <div class="sport_model">
+                                    <img src="logos/<?php echo $sports[$i]["imageurl"] ?>" id="sportImage<?php echo $sports[$i]["id"] ?>" alt="<?php echo $sports[$i]["enTitle"] ?>">
+                                </div>
+                                <h3 id="sportTitle<?php echo $sports[$i]["id"] ?>"><?php echo direction($sports[$i]["enTitle"],$sports[$i]["arTitle"]) ?></h3>
+                            </a>
+                        </div>
+					<?php
+						}
+					}
+					?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
