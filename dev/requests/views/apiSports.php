1ffe09bc58022efe9eeae9f089d3e6576ab9a69d
MRxwr <nasserhatab@gmail.com>
2024-10-16 21:31:38 +0300

    Update apiSports.php

--- dev/requests/views/apiSports.php
+++ dev/requests/views/apiSports.php
@@ -10,9 +10,9 @@ if( !isset($_GET["countryCode"]) || empty($_GET["countryCode"]) ){
     );
     echo json_encode($response);die();
 }
-if( $sports = selectDB2("`sports`","{$table}","`country` LIKE '{$_GET["countryCode"]}' AND `hidden` = '0' AND `status` = '0' GROUP BY `sport`") ){
+if( $sports = selectDB2("`sport`","{$table}","`country` LIKE '{$_GET["countryCode"]}' AND `hidden` = '0' AND `status` = '0' GROUP BY `sport`") ){
     for( $i = 0; $i < sizeof($sports); $i++ ){
-        $sport = selectDB("sports","`id` = '{$sports[$i]["sports"]}'");
+        $sport = selectDB("sports","`id` = '{$sports[$i]["sport"]}'");
         $response["sports"][] = array(
             "id" => $sport[0]["id"],
             "sportEn" => $sport[0]["enTitle"],
