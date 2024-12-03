47e82d9b611afd91ff2670f2464a62e9b7f32ebd
MRxwr <nasserhatab@gmail.com>
2024-12-04 00:03:24 +0300

    Update apiPayment.php

--- requests/views/apiPayment.php
+++ requests/views/apiPayment.php
@@ -129,15 +129,15 @@ if( !isset($_POST) ){
 
         //calculate totals prices that should be sent to upayments 
         if( $data["paymentMethod"] == 1 ){
-            $myacadDeposit = ( $academyData[0]["chargeType"] == "fixed" ) ? $academyData[0]["charges"] : $newTotal * ( $academyData[0]["charges"] / 100 );
+            $myacadDeposit = 0;//( $academyData[0]["chargeType"] == "fixed" ) ? $academyData[0]["charges"] : $newTotal * ( $academyData[0]["charges"] / 100 );
             $newTotal = $newTotal - $myacadDeposit;
             $paymentGateway = "knet";
         }elseif( $data["paymentMethod"] == 2 ){
-            $myacadDeposit = ( $academyData[0]["cc_chargetype"] == "fixed" ) ? $academyData[0]["cc_charge"] : $newTotal * ( $academyData[0]["cc_charge"] / 100 );
+            $myacadDeposit = 0;//( $academyData[0]["cc_chargetype"] == "fixed" ) ? $academyData[0]["cc_charge"] : $newTotal * ( $academyData[0]["cc_charge"] / 100 );
             $newTotal = $newTotal - $myacadDeposit;
             $paymentGateway = "cc";
         }else{
-            $myacadDeposit = 1;
+            $myacadDeposit = 0;//1;
             $newTotal = $newTotal - $myacadDeposit;
             $paymentGateway = "knet";
         }
@@ -157,18 +157,12 @@ if( !isset($_POST) ){
             'returnUrl' => 'https://myacad.app/index.php',
             'cancelUrl' => 'https://myacad.app/index.php',
             'notificationUrl' => 'https://myacad.app/index.php',
-            'extraMerchantData[0][amount]' => (string)$myacadDeposit,
+            'extraMerchantData[0][amount]' => (string)$fullAmount,
             'extraMerchantData[0][knetCharge]' => '0.25',
             'extraMerchantData[0][knetChargeType]' => 'fixed',
             'extraMerchantData[0][ccCharge]' => '0.25',
             'extraMerchantData[0][ccChargeType]' => 'fixed',
-            'extraMerchantData[0][ibanNumber]' => "{$AdminSettings[0]["mainIban"]}",
-            'extraMerchantData[1][amount]' => (string)($newTotal+(float)$jersyPrice),
-            'extraMerchantData[1][knetCharge]' => '0.25',
-            'extraMerchantData[1][knetChargeType]' => 'fixed',
-            'extraMerchantData[1][ccCharge]' => '0.25',
-            'extraMerchantData[1][ccChargeType]' => 'fixed',
-            'extraMerchantData[1][ibanNumber]' => "{$academyData[0]["iban"]}",
+            'extraMerchantData[0][ibanNumber]' => "{$academyData[0]["iban"]}",
             );
             
     }else{
@@ -343,7 +337,7 @@ if( !isset($_POST) ){
         CURLOPT_CUSTOMREQUEST => 'POST',
         CURLOPT_POSTFIELDS => $postBody,
         CURLOPT_HTTPHEADER => array(
-            'Authorization: Bearer afmceR6nHQaIehhpOel036LBhC8hihuB8iNh9ACF',
+            'Authorization: Bearer 779475522c0938b3c0774da98197e0727fefe464',
         ),
     ));
     $response = curl_exec($curl);
