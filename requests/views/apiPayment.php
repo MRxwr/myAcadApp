600d54322236c91b06715938df5e7c19bc10b423
MRxwr <nasserhatab@gmail.com>
2024-12-04 00:04:32 +0300

    Update apiPayment.php

--- requests/views/apiPayment.php
+++ requests/views/apiPayment.php
@@ -344,6 +344,8 @@ if( !isset($_POST) ){
     curl_close($curl);
     $response = json_decode($response,true);
     
+    var_dump($response);
+    var_dump($postBody);
     //saving info and redirecting to payment pages
     if( isset($response["status"]) && $response["status"] == true && isset($response["data"]["link"]) && !empty($response["data"]["link"]) ){
         $_POST["gatewayId"]     = $orderId;
