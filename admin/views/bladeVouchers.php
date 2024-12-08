e0bd6b8fc2af2c48e6480a2322edeb51c78e9745
MRxwr <nasserhatab@gmail.com>
2024-12-08 12:19:31 +0300

    Update bladeVouchers.php

--- admin/views/bladeVouchers.php
+++ admin/views/bladeVouchers.php
@@ -118,8 +118,7 @@
 		$count = (is_array($academiesList) && !empty($academiesList)) ? count($academiesList) : 1;
 		for( $z = 0; $z < $count; $z++ ){
 			$id = ( isset($academiesList[$z]) && !empty($academiesList[$z]) ) ? "AND `academyIds` LIKE '%{$academiesList[$z]}%'" : "AND `id` != '0'";
-			echo $id;
-		if( $vouchers = selectDB("vouchers","`status` = '0' AND `hidden` != '2' AND {$id}") ){
+		if( $vouchers = selectDB("vouchers","`status` = '0' AND `hidden` != '2' {$id}") ){
 				if ( $vouchers[0]["hidden"] == 1 ){
 					$icon = "fa fa-unlock";
 					$link = "?v={$_GET["v"]}&show={$vouchers[0]["id"]}";
