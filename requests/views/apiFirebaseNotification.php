012215f48ec76dc7d316d51e9a0634fa05b090ac
MRxwr <nasserhatab@gmail.com>
2024-12-22 09:29:16 +0300

    Update apiFirebaseNotification.php

--- requests/views/apiFirebaseNotification.php
+++ requests/views/apiFirebaseNotification.php
@@ -52,6 +52,7 @@ if( $users = selectDB("users", "`id` = '6' GROUP BY `firebase` ORDER BY `id` ASC
           ),
         ));
         $response = curl_exec($curl);
+        var_dump($response);
         curl_close($curl);
     }
 }
