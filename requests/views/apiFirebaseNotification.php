9f4640c96ebe15fd067abeb47e8c86ac599236fe
MRxwr <nasserhatab@gmail.com>
2024-12-22 09:22:31 +0300

    Update apiFirebaseNotification.php

--- requests/views/apiFirebaseNotification.php
+++ requests/views/apiFirebaseNotification.php
@@ -17,6 +17,7 @@ function getAccessToken() {
     ));
     $response = curl_exec($curl);
     $response = json_decode($response, true);
+    var_Dump($response);
     curl_close($curl);
     return $response["data"]['access_token'];
 }
