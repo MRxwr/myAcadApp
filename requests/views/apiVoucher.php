4738be81ff54165edc66861ff3f087bdbb249cc4
MRxwr <nasserhatab@gmail.com>
2024-12-30 01:06:11 +0300

    Update apiVoucher.php

--- requests/views/apiVoucher.php
+++ requests/views/apiVoucher.php
@@ -36,7 +36,8 @@ if( isset($_POST["code"]) && !empty($_POST["code"]) && $voucher = selectDB("vouc
     
     if( $voucher[0]["academyIds"] != 0 ){
         $voucher[0]["academyIds"] = json_decode($voucher[0]["academyIds"],true);
-        if(  in_array($_POST["academyId"],$voucher[0]["academyIds"]) ){
+        var_dump($voucher[0]["academyIds"]);die();
+        if( in_array($_POST["academyId"],$voucher[0]["academyIds"]) ){
             $academyAprroved = true;
         }else{
             $academyAprroved = false;
