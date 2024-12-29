15ba24fe0e8764b077ab13e5dcbb0a44cee6fd10
MRxwr <nasserhatab@gmail.com>
2024-12-30 01:09:36 +0300

    Update apiVoucherTournament.php

--- requests/views/apiVoucherTournament.php
+++ requests/views/apiVoucherTournament.php
@@ -3,6 +3,13 @@ $numberOfTimesAvalability = false;
 $tournamentApproved = false;
 $dateApproved = false;
 if( isset($_POST["code"]) && !empty($_POST["code"]) && $voucher = selectDB("vouchers","`code` = '{$_POST["code"]}' AND `typeOfVoucher` = '1' AND `hidden` = '0' AND `status` = '0'")){
+    if( !isset($_POST["tournamentId"]) || empty($_POST["tournamentId"]) ){
+        $response = array(
+            "msg" => 'tournament is required.',
+            "msgAr" => 'يجب إدخال البطولة',
+        );
+        echo outputError($response);die();
+    }
     $currentDate = date("Y-m-d");
     if( (substr($voucher[0]["startDate"],0,10) <= $currentDate) && (substr($voucher[0]["endDate"],0,10) >= $currentDate) ){
         $dateApproved = true;
@@ -34,7 +41,7 @@ if( isset($_POST["code"]) && !empty($_POST["code"]) && $voucher = selectDB("vouc
         }
     }
     
-    if( $voucher[0]["tournamentIds"] != 0 ){
+    if( !empty($voucher[0]["tournamentIds"]) ){
         $voucher[0]["tournamentIds"] = json_decode($voucher[0]["tournamentIds"],true);
         if(  in_array($_POST["tournamentId"],$voucher[0]["tournamentIds"]) ){
             $tournamentApproved = true;
