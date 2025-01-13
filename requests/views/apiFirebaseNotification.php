be1f49957009bcb6ea873b5d318408a74e40b393
MRxwr <nasserhatab@gmail.com>
2025-01-13 23:37:39 +0300

    Update apiFirebaseNotification.php

--- requests/views/apiFirebaseNotification.php
+++ requests/views/apiFirebaseNotification.php
@@ -1,5 +1,6 @@
 <?php
 function sendToTopic($data){
+    /*
     $curl = curl_init();
     curl_setopt_array($curl, array(
     CURLOPT_URL => 'https://createapi.link/api/v1/send_to_topic.php',
@@ -20,6 +21,29 @@ function sendToTopic($data){
     $response = curl_exec($curl);
     curl_close($curl);
     return  $response;
+*/
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://myacad.app/requests?a=Firebase&action=sendNotification',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => array(
+        'title' => "{$data["title"]}",
+        'body' => "{$data["body"]}",
+        'image' => "",
+    ),
+    CURLOPT_HTTPHEADER => array(
+        'myacadheader: myAcadAppCreate'
+    ),
+    ));
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return $response;
 }
 
 sendToTopic($_POST);
