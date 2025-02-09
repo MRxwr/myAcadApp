ea436d64c856e7be1f0acb512045bbcfa93874cc
MRxwr <nasserhatab@gmail.com>
2025-02-09 16:11:00 +0300

    Update bladeWarnings.php

--- admin/views/bladeWarnings.php
+++ admin/views/bladeWarnings.php
@@ -31,7 +31,7 @@
             "on" => ["t.academyId = t1.id","t1.area = t2.id"]
         );
         // add where to the quesy to check if the data passed 30 days or not
-			if( $academies = selectJoinDB("orders",$joinData,"t.date <= DATE_SUB(NOW(), INTERVAL 30 DAY) AND (t.status = '1' OR t.status = '4') AND t.tournamentId = '0' GROUP BY t.academyId ORDER BY t1.id DESC") ){
+			if( $academies = selectJoinDB("orders",$joinData,"t.date <= DATE_SUB(NOW(), INTERVAL 30 DAY) AND (t.status = '1' OR t.status = '4') AND t.tournamentId = '0' GROUP BY t.academyId ORDER BY t.id DESC") ){
                 $gendersList = array(
                     "1" => direction("Man","رجل"),
                     "2" => direction("Woman","إمرأه"),
