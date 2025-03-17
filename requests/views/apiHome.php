4851558ee53bd80632277ebd3aedceae53a296e3
MRxwr <nasserhatab@gmail.com>
2025-03-17 05:22:05 +0300

    Update apiHome.php

--- requests/views/apiHome.php
+++ requests/views/apiHome.php
@@ -15,7 +15,7 @@ if( $sports = selectDB2("`id`, `enTitle`, `arTitle`, `imageurl`","sports","`hidd
 }
 
 $gendersEn = ["SELECT GENDER","Man","Woman","Boy","Girl","Mix Adults","Mix Kids"];
-$gendersAr = ["إختيار الجنس","رجل","إمرأة","ولد","بنت","مختلط كبار","مختلط الاطفال"];
+$gendersAr = ["إختيار الجنس","رجال","سيدات","أولاد","بنات","مختلط كبار","مختلط الاطفال"];
 for( $i = 0; $i < sizeof($gendersEn); $i++ ){
     $response["genders"][] = array("genderEn" => $gendersEn[$i], "genderAr" => $gendersAr[$i]);
 }
