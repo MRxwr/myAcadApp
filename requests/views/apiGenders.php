4028e1eaeb888db8e466c9c27d32edb691d60275
MRxwr <nasserhatab@gmail.com>
2025-03-17 05:24:05 +0300

    Update apiGenders.php

--- requests/views/apiGenders.php
+++ requests/views/apiGenders.php
@@ -18,7 +18,7 @@ if( !isset($_GET["countryCode"]) || empty($_GET["countryCode"]) ){
 }
 if( $academies = selectDB2("`gender`","{$table}","`sport` = '{$_GET["sportId"]}' AND `country` LIKE '{$_GET["countryCode"]}' AND `hidden` = '0' AND `status` = '0' GROUP BY `gender`") ){
     $gendersEn = ["SELECT Gender","Man","Woman","Boy","Girl","Mix Adults","Mix Kids"];
-    $gendersAr = ["إختيار الجنس","رجل","إمرأة","ولد","بنت","مختلط كبار","مختلط الاطفال"];
+    $gendersAr = ["إختيار الجنس","رجال","سيدات","أولاد","بنات","مختلط كبار","مختلط الاطفال"];
     $response["genders"][0] = array(
         "id" => 0,
         "genderEn" => $gendersEn[0],
