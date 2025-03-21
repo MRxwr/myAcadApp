86bfeb6e1c0f131b8f02015a9f67dfdddaab42f6
MRxwr <nasserhatab@gmail.com>
2025-03-21 05:58:55 +0300

    Update sql.php

--- admin/includes/functions/sql.php
+++ admin/includes/functions/sql.php
@@ -147,7 +147,7 @@ function selectJoinDB($table, $joinData, $where){
         }
     }else{
         $error = array("msg"=>"select table error");
-        return outputError($error);
+        return 0;
     }
 }
 
