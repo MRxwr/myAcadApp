9b6f3b285d22ae0fc11cee7d7ef684989713b94a
MRxwr <nasserhatab@gmail.com>
2025-03-29 21:50:31 +0300

    Update apiFirebaseNotification.php

--- requests/views/apiFirebaseNotification.php
+++ requests/views/apiFirebaseNotification.php
@@ -81,7 +81,7 @@ $notificationData = array(
     )
 );
 
-if( $users = selectDB("users", "`id` != '0' GROUP BY `firebase` ORDER BY `id` ASC") ){
+if( $users = selectDB("users", "`id` = '665' GROUP BY `firebase` ORDER BY `id` ASC") ){
     for( $i = 0; $i < sizeof($users); $i++){
         $notificationData["message"]["token"] = $users[$i]["firebase"];
         $curl = curl_init();
