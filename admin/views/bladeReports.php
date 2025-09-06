25b6ec296f36192495c2264730b21f8b5ccebb4b
MRxwr <nasserhatab@gmail.com>
2025-09-07 01:20:14 +0300

    Update bladeReports.php

--- admin/views/bladeReports.php
+++ admin/views/bladeReports.php
@@ -38,7 +38,7 @@
 		$orderBy = direction("enTitle","arTitle");
 		for( $z = 0; $z < $count; $z++ ){
 			$id = ( isset($academiesList[$z]) && !empty($academiesList[$z]) ) ? "AND `id` = '{$academiesList[$z]}'" : "AND `id` != '0'";
-			if( $academies = selectDB("academies","`status` = '0' {$id} ORDER BY `{$orderBy}` ASC") ){
+			if( $academies = selectDB("academies","`status` != '4' {$id} ORDER BY `{$orderBy}` ASC") ){
 				for( $i = 0; $i < sizeof($academies); $i++ ){
 					$area = selectDB("countries","`id` = '{$academies[$i]["area"]}'");
 					$areaTitle = direction($area[0]["areaEnTitle"],$area[0]["areaArTitle"]);
