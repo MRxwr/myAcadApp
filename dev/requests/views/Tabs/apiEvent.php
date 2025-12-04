4987bf960f29ff966ae9bb1d39cafc6e782d2f91
MRxwr <nasserhatab@gmail.com>
2025-12-04 05:26:36 +0300

    save

--- dev/requests/views/Tabs/apiEvent.php
+++ dev/requests/views/Tabs/apiEvent.php
@@ -3,7 +3,7 @@ if( !isset($_GET["eventId"]) || empty($_GET["eventId"]) ){
 	$response = array("msg"=>"Please set event id");
 	echo outputError($response);die();
 }else{
-	if( $event = selectDB2("`id`, `imageurl`, `enTitle`, `arTitle`, `area`, `video`, `location`, `price`, `players`, `bench`, `locationImage`, `enTerms`, `arTerms`, `gameDate`, `gameTime`","tabs_list","`hidden` = '0' AND `status` = '0' AND `id` = {$_GET["eventId"]}") ){
+	if( $event = selectDB2("`id`, `imageurl`, `enTitle`, `arTitle`, `area`, `video`, `location`, `price`, `locationImage`, `enTerms`, `arTerms`, `gameDate`, `gameTime`","tabs_list","`hidden` = '0' AND `status` = '0' AND `id` = {$_GET["eventId"]}") ){
 		$response["event"] = $event[0];
 		$response["event"]["video"] = ( !empty($response["event"]["video"]) ) ? "https://www.youtube.com/embed/{$event[0]["video"]}" : "";
 		if( $area = selectDB("countries","`id` = '{$event[0]["area"]}'") ){
