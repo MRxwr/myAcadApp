36dedc0ea2084f8ea68a8b70eaaab2029c5c52ae
MRxwr <nasserhatab@gmail.com>
2025-12-04 05:30:20 +0300

    Update apiEvent.php

--- dev/requests/views/Tabs/apiEvent.php
+++ dev/requests/views/Tabs/apiEvent.php
@@ -3,10 +3,10 @@ if( !isset($_GET["eventId"]) || empty($_GET["eventId"]) ){
 	$response = array("msg"=>"Please set event id");
 	echo outputError($response);die();
 }else{
-	if( $event = selectDB2("`id`, `imageurl`, `enTitle`, `arTitle`, `area`, `video`, `location`, `price`, `locationImage`, `enTerms`, `arTerms`, `gameDate`, `gameTime`","tabs_list","`hidden` = '0' AND `status` = '0' AND `id` = {$_GET["eventId"]}") ){
+	if( $event = selectDB2("`id`, `imageurl`, `enTitle`, `arTitle`, `area`, `video`, `location`, `price`, `locationImage`, `enTerms`, `arTerms`, `gameDate`, `gameTime`, `fieldsIds`","tabs_list","`hidden` = '0' AND `status` = '0' AND `id` = {$_GET["eventId"]}") ){
 		$response["event"] = $event[0];
 		$response["event"]["video"] = ( !empty($response["event"]["video"]) ) ? "https://www.youtube.com/embed/{$event[0]["video"]}" : "";
-        $feildsToBeFilled = json_decode( $event[0]["fieldsIds"], true );
+        $fieldsToBeFilled = json_decode( $event[0]["fieldsIds"], true );
 		if( $area = selectDB("countries","`id` = '{$event[0]["area"]}'") ){
 			$response["event"]["enArea"] = $area[0]["areaEnTitle"];
 			$response["event"]["arArea"] = $area[0]["areaArTitle"];
@@ -15,8 +15,8 @@ if( !isset($_GET["eventId"]) || empty($_GET["eventId"]) ){
 			$response["event"]["arArea"] = "";
 		}
         $response["event"]["fieldsToBeFilled"] = array();
-        if( !empty( $feildsToBeFilled ) ){
-            foreach( $feildsToBeFilled as $fieldId ){
+        if( !empty( $fieldsToBeFilled ) ){
+            foreach( $fieldsToBeFilled as $fieldId ){
                 if( $field = selectDB("tabs_fields","`id` = '{$fieldId}'") ){
                     $response["event"]["fieldsToBeFilled"][] = array(
                         "id" => $field[0]["id"],
