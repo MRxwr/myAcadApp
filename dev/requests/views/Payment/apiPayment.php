d26db99001f7b5473b7d71b76485f4494c9ca1d8
MRxwr <nasserhatab@gmail.com>
2025-12-14 23:45:41 +0300

    Update apiPayment.php

--- dev/requests/views/Payment/apiPayment.php
+++ dev/requests/views/Payment/apiPayment.php
@@ -500,7 +500,7 @@ if( !isset($_POST) ){
     
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://sandboxapi.upayments.com/api/v1/charge',
+        CURLOPT_URL => 'https://uapi.upayments.com/api/v1/charge',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -510,13 +510,13 @@ if( !isset($_POST) ){
         CURLOPT_CUSTOMREQUEST => 'POST',
         CURLOPT_POSTFIELDS => $postBody,
         CURLOPT_HTTPHEADER => array(
-            'Authorization: Bearer jtest123',
+            'Authorization: Bearer 779475522c0938b3c0774da98197e0727fefe464',
         ),
     ));
     $response = curl_exec($curl);
     curl_close($curl);
     $response = json_decode($response,true);
-    var_dump($response);
+    
     //saving info and redirecting to payment pages
     if( isset($response["status"]) && $response["status"] == true && isset($response["data"]["link"]) && !empty($response["data"]["link"]) ){
         $_POST["gatewayId"]     = $orderId;
