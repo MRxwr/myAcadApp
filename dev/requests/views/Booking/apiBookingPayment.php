02ac332cd5fd337850cca87c7cdcf697788f2e46
MRxwr <nasserhatab@gmail.com>
2025-12-15 00:06:40 +0300

    Update apiBookingPayment.php

--- dev/requests/views/Booking/apiBookingPayment.php
+++ dev/requests/views/Booking/apiBookingPayment.php
@@ -8,6 +8,7 @@ if( !isset($_POST) ){
     $data = $_POST;
     unset($_POST);
     if( isset($data["fieldId"]) && !empty($data["fieldId"]) ){
+        var_dump($data);
         $user = $data["user"];
         $paymentMethod = $data["paymentMethod"];
     
