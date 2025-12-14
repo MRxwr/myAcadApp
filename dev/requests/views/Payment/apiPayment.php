a3ee0a3430a65802e5f3f0c39447855f89cf2a41
MRxwr <nasserhatab@gmail.com>
2025-12-14 23:35:29 +0300

    Update apiPayment.php

--- dev/requests/views/Payment/apiPayment.php
+++ dev/requests/views/Payment/apiPayment.php
@@ -510,7 +510,7 @@ if( !isset($_POST) ){
         CURLOPT_CUSTOMREQUEST => 'POST',
         CURLOPT_POSTFIELDS => $postBody,
         CURLOPT_HTTPHEADER => array(
-            'Authorization: Bearer e66a94d579cf75fba327ff716ad68c53aae11528',
+            'Authorization: Bearer jtest123',
         ),
     ));
     $response = curl_exec($curl);
