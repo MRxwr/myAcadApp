7154ab26550d7f0aa09cad3491d7fcc954381bdf
MRxwr <nasserhatab@gmail.com>
2025-12-31 02:48:18 +0300

    Update apiBookingPayment.php

--- dev/requests/views/Booking/apiBookingPayment.php
+++ dev/requests/views/Booking/apiBookingPayment.php
@@ -109,6 +109,7 @@ if( !isset($_POST) ){
     ));
     $response = curl_exec($curl);
     curl_close($curl);
+    print_r(json_encode($response));die();
     $response = json_decode($response,true);
 
     //saving info and redirecting to payment pages
