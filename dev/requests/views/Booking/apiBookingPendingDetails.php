527d8a8604c7fea4a59e9990da8d2a352be39581
MRxwr <nasserhatab@gmail.com>
2025-12-31 03:38:25 +0300

    save

--- dev/requests/views/Booking/apiBookingPendingDetails.php
+++ dev/requests/views/Booking/apiBookingPendingDetails.php
@@ -6,14 +6,18 @@ if( !isset($_POST["id"]) || empty($_POST["id"]) ){
 $data = $_POST;
 unset($_POST);
 $dataJoin = array(
-    "select" => ["t.id","t.bookingDate","t.startTime","t.endTime","t.notes", "t1.enTitle as fieldEnTitle","t1.arTitle as fieldArTitle","t1.imageurl as fieldLogo","t2.enTitle as ageEnTitle","t2.arTitle as ageArTitle","t3.enTitle as levelEnTitle","t3.arTitle as levelArTitle","t4.areaEnTitle as areaEnTitle","t4.areaArTitle as areaArTitle","t5.enTitle as sportEnTitle","t5.arTitle as sportArTitle", "(CASE WHEN t.status = 0 THEN 'Pending' WHEN t.status = 1 THEN 'Partialy Paid' WHEN t.status = 2 THEN 'Fully Paid' ELSE '' END) as enStatus", "(CASE WHEN t.status = 0 THEN 'إنتظار' WHEN t.status = 1 THEN 'مدفوع نص المبلغ' WHEN t.status = 2 THEN 'مدفوع كامل' ELSE '' END) as arStatus", "t1.facilities"],
-    "join" => ["fields_list","field_ages","field_levels","countries","sports","field_facilities"],
-    "on" => ["t.fieldId = t1.id","t.ages = t2.id","t.levels = t3.id","t1.area = t4.id","t1.sport = t5.id","t1.facilitiesIds = t6.id"],
+    "select" => ["t.id","t.bookingDate","t.startTime","t.endTime","t.notes", "t1.enTitle as fieldEnTitle","t1.arTitle as fieldArTitle","t1.imageurl as fieldLogo","t2.enTitle as ageEnTitle","t2.arTitle as ageArTitle","t3.enTitle as levelEnTitle","t3.arTitle as levelArTitle","t4.areaEnTitle as areaEnTitle","t4.areaArTitle as areaArTitle","t5.enTitle as sportEnTitle","t5.arTitle as sportArTitle", "(CASE WHEN t.status = 0 THEN 'Pending' WHEN t.status = 1 THEN 'Partialy Paid' WHEN t.status = 2 THEN 'Fully Paid' ELSE '' END) as enStatus", "(CASE WHEN t.status = 0 THEN 'إنتظار' WHEN t.status = 1 THEN 'مدفوع نص المبلغ' WHEN t.status = 2 THEN 'مدفوع كامل' ELSE '' END) as arStatus", "t1.facilitiesIds"],
+    "join" => ["fields_list","field_ages","field_levels","countries","sports"],
+    "on" => ["t.fieldId = t1.id","t.ages = t2.id","t.levels = t3.id","t1.area = t4.id","t1.sport = t5.id"],
 );
 if( $pendingBooking = selectJoinDB("fields_booking", $dataJoin, "t.id = '{$data["id"]}'") ){
-    $pendingBooking[0]["facilities"] = explode(",",$pendingBooking[0]["facilitiesIds"]);
-    $response["pendingBooking"] = $pendingBooking;
-
+    $facilities = json_decode($pendingBooking[0]["facilitiesIds"], true);
+    $pendingBooking[0]["facilities"] = array();
+    if( !empty($facilities) && is_array($facilities) ){
+        $facilitiesIds = implode(',', array_map('intval', $facilities));
+        $pendingBooking[0]["facilities"] = selectDB2("enTitle, arTitle, imageurl","field_facilities","`id` IN ({$facilitiesIds})");
+    }
+    $response["pendingBooking"] = $pendingBooking[0];
 }else{
     $response["msg"] = popupMsg($requestLang,"No Bookings available","لا يوجد مباريات متاحة");
     echo outputError($response);die();
