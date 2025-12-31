68e3e39353ec532244510c1b8ceedd97928e2096
MRxwr <nasserhatab@gmail.com>
2025-12-31 04:46:36 +0300

    save

---
+++ dev/booking/index.php
@@ -0,0 +1,29 @@
+<?php
+if( isset($_GET["requested_order_id"]) && !empty($_GET["requested_order_id"]) ){
+    if( $booking = selectDBNew("fields_booking",[$_GET["requested_order_id"]],"gatewayId = ?","") ){
+        if( isset($_GET["result"]) && !empty($_GET["result"]) ){
+            if( $_GET["result"] == "CAPTURED" ){
+                updateDB("fields_booking",array("status" => 2),"`gatewayId` = '{$_GET["requested_order_id"]}'");
+                if( $booking[0]["isTbari"] == 1 ){
+                    if( $bookingOriginal = selectDBNew("fields_booking",[$booking[0]["bookingId"]],"id = ?","") ){
+                        if( $bookingOriginal[0]["status"] == 0 ){
+                            updateDB("fields_booking",array("status" => 1),"`id` = '{$bookingOriginal[0]["id"]}'");
+                        }elseif( $bookingOriginal[0]["status"] == 1 ){
+                            updateDB("fields_booking",array("status" => 2),"`id` = '{$bookingOriginal[0]["id"]}'");
+                        }
+                    }else{
+                        die("404 Not Found [ Original Booking Not Found ]");
+                    }
+                }
+            }else{
+                updateDB("fields_booking",array("status" => 4),"`gatewayId` = '{$_GET["requested_order_id"]}'");
+            }
+        }else{
+            die("404 Not Found [ Result Not Found ]");
+        }
+    }else{
+        die("404 Not Found [ Booking Not Found ]");
+    }
+}else{
+    die("404 Not Found [ Requested Order ID Not Found ]");
+}
