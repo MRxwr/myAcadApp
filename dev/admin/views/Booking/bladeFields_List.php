a5d027c72f4936f581330713f251e267daf6142e
MRxwr <nasserhatab@gmail.com>
2026-02-21 05:05:29 +0300

    Update bladeFields_List.php

--- dev/admin/views/Booking/bladeFields_List.php
+++ dev/admin/views/Booking/bladeFields_List.php
@@ -215,17 +215,17 @@
 			</div>
 
 			<div id="images" style="margin-top: 10px; display:none">
-				<div class="col-md-3">
+				<div class="col-md-4">
 				<label><?php echo direction("Logo Preview","معاينة الشعار") ?></label>
 				<img id="logoImg" src="" style="width:100%;height:200px;object-fit:contain;border:1px solid #ddd;padding:5px;border-radius:5px">
 				</div>
 
-				<div class="col-md-3">
+				<div class="col-md-4">
 				<label><?php echo direction("Map Preview","معاينة الخريطة") ?></label>
 				<img id="locationImg" src="" style="width:100%;height:200px;object-fit:contain;border:1px solid #ddd;padding:5px;border-radius:5px">
 				</div>
 
-				<div class="col-md-6">
+				<div class="col-md-4">
 				<label><?php echo direction("Header Images","صور الهيدر") ?></label>
 				<div id="header_previews_container" class="row">
 					<!-- Header images will be listed here -->
