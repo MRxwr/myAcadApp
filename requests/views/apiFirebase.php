53a30fa7c94b5c53fbb50d3d4be9f747ccf593d4
MRxwr <nasserhatab@gmail.com>
2026-02-23 02:08:32 +0300

    Update apiFirebase.php

--- requests/views/apiFirebase.php
+++ requests/views/apiFirebase.php
@@ -1,6 +1,7 @@
 <?php
 if( isset($_GET["action"]) & !empty($_GET["action"]) ){
     if( $_GET["action"] == "register" ){
+        print_r($_REQUEST);die();
         if( !isset($_POST["deviceToken"]) || empty($_POST["deviceToken"]) ){
             $error["msg"] = popupMsg($requestLang,"Please enter device token . {$_POST["deviceToken"]}","الرجاء ادخال رمز الجهاز");
             echo outputError($error);die();
