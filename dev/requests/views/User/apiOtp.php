1f6f7e2c9c1c89d803b35ae3f80b1dfa1d1f67aa
MRxwr <nasserhatab@gmail.com>
2026-02-23 01:28:50 +0300

    Update apiOtp.php

--- dev/requests/views/User/apiOtp.php
+++ dev/requests/views/User/apiOtp.php
@@ -9,24 +9,15 @@ if( isset($_GET["type"]) && !empty($_GET["type"]) ){
             $response["msg"] = "Please provide a mobile number.";
             echo outputError($response);die();
         }
-        if( $userMobile = selectDBNew("user", [$_POST["mobile"]], "`mobile` LIKE CONCAT('%', ?, '%') AND `isVerified` = '1'","") ){
-            $response["msg"] = "Mobile number is already in use.";
-            echo outputError($response);die();
-        }
         if( $user = selectDB2("`mobile`, `isVerified`", "user", "`id` = '{$_POST["userId"]}'") ){
-            if( $user[0]["isVerified"] == 1 ){
-                $response["msg"] = "User is already verified.";
+            $otp = rand(1000, 9999);
+            if( updateDB("user", ["otp" => $otp], "`id` = '{$_POST["userId"]}'" ) ){
+                whatsappUltraMsgVerify($_POST["mobile"], $otp);
+                $response["msg"] = "OTP sent to your mobile.";
                 echo outputData($response);die();
             }else{
-                $otp = rand(1000, 9999);
-                if( updateDB("user", ["otp" => $otp], "`id` = '{$_POST["userId"]}'" ) ){
-                    whatsappUltraMsgVerify($_POST["mobile"], $otp);
-                    $response["msg"] = "OTP sent to your mobile.";
-                    echo outputData($response);die();
-                }else{
-                    $response["msg"] = "Failed to send OTP.";
-                    echo outputError($response);die();
-                }
+                $response["msg"] = "Failed to send OTP.";
+                echo outputError($response);die();
             }
         }else{
             $response["msg"] = "User not found.";
