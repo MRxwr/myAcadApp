0628d9767fa12ded060e9b23d8074cc9b1aba1b8
MRxwr <nasserhatab@gmail.com>
2026-02-26 04:42:52 +0300

    save

--- dev/requests/views/Booking/apiBookingList.php
+++ dev/requests/views/Booking/apiBookingList.php
@@ -14,8 +14,8 @@ $dataJoin = array(
     "on" => ["t.fieldId = t1.id","t.ages = t2.id","t.levels = t3.id","t1.area = t4.id","t1.sport = t5.id"],
     "type" => ["left","left","left","left","left","left"],
 );
-if( $bookingList = selectJoinDB("fields_booking", $dataJoin, "t.userId = '{$data["userId"]}' AND t.bookingId = '0' ORDER BY t.id DESC LIMIT {$limit} OFFSET {$offset}") ){
-    $response["bookingList"] = $bookingList;
+if( $bookingList = selectJoinDB("fields_booking", $dataJoin, "t.userId = '{$data["userId"]}' ORDER BY t.id DESC LIMIT {$limit} OFFSET {$offset}") ){
+    $response["bookingList"] = $bookingList; //AND t.bookingId = '0'
 }else{
     $response["msg"] = popupMsg($requestLang,"No Bookings available","لا يوجد مباريات متاحة");
     echo outputError($response);die();
