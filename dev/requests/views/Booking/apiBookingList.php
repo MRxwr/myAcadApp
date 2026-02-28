6626fbcb85975d8a470f2d572852cf281b70a782
MRxwr <nasserhatab@gmail.com>
2026-02-28 04:02:28 +0300

    Update apiBookingList.php

--- dev/requests/views/Booking/apiBookingList.php
+++ dev/requests/views/Booking/apiBookingList.php
@@ -9,7 +9,7 @@ $page = ( isset($_GET["page"]) && !empty($_GET["page"]) ) ? intval($_GET["page"]
 $limit = 10;
 $offset = ($page - 1) * $limit;
 $dataJoin = array(
-    "select" => ["t.id","t.bookingDate","t.startTime","t.endTime","t1.enTitle as fieldEnTitle","t1.arTitle as fieldArTitle","t1.imageurl as fieldLogo","t2.enTitle as ageEnTitle","t2.arTitle as ageArTitle","t3.enTitle as levelEnTitle","t3.arTitle as levelArTitle","t4.areaEnTitle as areaEnTitle","t4.areaArTitle as areaArTitle","t5.enTitle as sportEnTitle","t5.arTitle as sportArTitle", "(CASE WHEN t.status = 0 THEN 'Pending' WHEN t.status = 1 THEN 'Partialy Paid' WHEN t.status = 2 THEN 'Fully Paid' ELSE '' END) as enStatus", "(CASE WHEN t.status = 0 THEN 'إنتظار' WHEN t.status = 1 THEN 'مدفوع نص المبلغ' WHEN t.status = 2 THEN 'مدفوع كامل' ELSE '' END) as arStatus"],
+    "select" => ["t.id","t.bookingDate","t.startTime","t.endTime","t1.enTitle as fieldEnTitle","t1.arTitle as fieldArTitle","t1.imageurl as fieldLogo","t2.enTitle as ageEnTitle","t2.arTitle as ageArTitle","t3.enTitle as levelEnTitle","t3.arTitle as levelArTitle","t4.areaEnTitle as areaEnTitle","t4.areaArTitle as areaArTitle","t5.enTitle as sportEnTitle","t5.arTitle as sportArTitle", "(CASE WHEN t.status = 0 THEN 'Pending' WHEN t.status = 1 THEN 'Partialy Paid' WHEN t.status = 2 THEN 'Fully Paid' ELSE '' END) as enStatus", "(CASE WHEN t.status = 0 THEN 'إنتظار' WHEN t.status = 1 THEN 'مدفوع نص المبلغ' WHEN t.status = 2 THEN 'مدفوع كامل' ELSE '' END) as arStatus","t.voucher","t1.total"],
     "join" => ["fields_list","field_ages","field_levels","countries","sports"],
     "on" => ["t.fieldId = t1.id","t.ages = t2.id","t.levels = t3.id","t1.area = t4.id","t1.sport = t5.id"],
     "type" => ["left","left","left","left","left","left"],
