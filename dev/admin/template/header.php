e8d664a31e8ad50c98273113f3cd61eb4f251433
MRxwr <nasserhatab@gmail.com>
2026-03-16 23:27:56 +0300

    Update header.php

--- dev/admin/template/header.php
+++ dev/admin/template/header.php
@@ -90,7 +90,7 @@ require ("includes/checksouthead.php");
 			100% { transform: rotate(360deg); }
 		}
 		.activeSidebar{
-			color:#ffa300 !important;
+			color: #f0b93f !important;
 		}
 	</style>
 
