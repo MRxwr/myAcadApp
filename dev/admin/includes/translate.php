80ac9a255c6ac4008e8ab234303cc7127dcad379
MRxwr <nasserhatab@gmail.com>
2026-03-16 23:26:15 +0300

    Update translate.php

--- dev/admin/includes/translate.php
+++ dev/admin/includes/translate.php
@@ -1,6 +1,6 @@
 <?php
 $cookieSession = "myAcad";
-$settingsTitle = "MY ACAD";
+$settingsTitle = "My Academy";
 $settingslogo = "logo.png";
 if ( isset($_GET["Lang"]) ){
 	$arrayLangs = ["EN","AR"];
